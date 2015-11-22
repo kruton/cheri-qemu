cc667177c6dec2ea5c5cab8cd42ae2e91a73fff7
Stacey Son <sson@FreeBSD.org>
2015-11-22 05:41:12 -0600

    Bug fix: Tag violation should return the right cause.

--- target/mips/tcg/op_helper.c
+++ target/mips/tcg/op_helper.c
@@ -212,7 +212,6 @@ static inline void check_hwrena(CPUMIPSState *env, int reg, uintptr_t pc)
     if ((env->hflags & MIPS_HFLAG_CP0) || (env->CP0_HWREna & (1 << reg))) {
         return;
     }
-    do_raise_exception(env, EXCP_RI, pc);
 }
 
 target_ulong helper_rdhwr_cpunum(CPUMIPSState *env)
