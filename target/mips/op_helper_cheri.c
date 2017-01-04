d212be1a8949c1ffac2aeabf39f32773e9051d74
Stacey Son <sson@FreeBSD.org>
2017-01-04 17:32:37 +0000

    Bug fix for the CUnseal instruction.
    The helper for the CUnseal was setting the permission incorrectly
    when the destination register and Ct are the same register. Brian
    Campbell found this bug with his automatic test generator.

--- target/mips/op_helper_cheri.c
+++ target/mips/op_helper_cheri.c
@@ -26,6 +26,7 @@ is_cap_sealed(const cap_register_t *cp)
     case CP2HWR_KCC:
     case CP2HWR_KDC:
     case CP2HWR_EPCC:
+    *cdp = *csp;
     } else if (is_cap_sealed(cbp)) {
         do_raise_c0_exception(env, EXCP_AdEL, addr);
     } else if (is_cap_sealed(cbp)) {
