4e30090dc9e5f587839b0eef718501e7fc820296
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-02-12 11:21:19 +0000

    Use CPUArchState instead of CPUMIPSState

---
+++ target/cheri-common/cheri_tagmem.h
@@ -0,0 +1,3 @@
+void cheri_tag_invalidate(CPUArchState *env, target_ulong vaddr, int32_t size,
+int cheri_tag_get_many(CPUArchState *env, target_ulong vaddr, int reg,
+void *cheri_tag_set(CPUArchState *env, target_ulong vaddr, int reg,
