04671d95410d2d8cba832e8cd0d3c1f6c58d90b2
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-02-16 17:29:06 +0000

    Only memset decompressed capregs to 0xaa with CONFIG_DEBUG_TCG

---
+++ target/cheri-common/cheri-lazy-capregs.h
@@ -0,0 +1,8 @@
+    }
+#ifdef CONFIG_DEBUG_TCG
+    sanity_check_capreg(gpcrs, regnum);
+#endif
+    case CREG_INTEGER: {
+        return result;
+                                 const cap_register_t *newval)
+    case CREG_INTEGER:
