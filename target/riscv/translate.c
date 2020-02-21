159db6b1db91281133e0b68f4409e87d0384f9f3
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-02-21 10:09:37 +0000

    Save 64bits of capapability register storage
    There is no need to duplicate the cursor. Even if the rest of the fields
    are invalid we can still save the cursor in the cap_register_t.

--- target/riscv/translate.c
+++ target/riscv/translate.c
@@ -1483,6 +1483,7 @@ void riscv_translate_init(void)
      */
     for (i = 1; i < 32; i++) {
         _cpu_cursors_do_not_access_directly[i] = tcg_global_mem_new(
+            offsetof(CPURISCVState, gpcapregs.decompressed[i].cap._cr_cursor),
             riscv_int_regnames[i]);
     }
 #endif
