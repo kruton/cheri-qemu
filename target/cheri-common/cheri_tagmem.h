ca9edc644dfb04bce250daecee1e323fde79639a
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-02-21 10:02:57 +0000

    Pass the RAM MemoryRegion* to cheri_tag_init()
    Nu functional change yet, but this will allow future refactoring of tag
    storage.

--- target/cheri-common/cheri_tagmem.h
+++ target/cheri-common/cheri_tagmem.h
@@ -1,3 +1,5 @@
+#include "system/memory.h"
+void cheri_tag_init(MemoryRegion* mr, uint64_t memory_size);
 void cheri_tag_invalidate(CPUArchState *env, target_ulong vaddr, int32_t size,
 int cheri_tag_get_many(CPUArchState *env, target_ulong vaddr, int reg,
 void *cheri_tag_set(CPUArchState *env, target_ulong vaddr, int reg,
