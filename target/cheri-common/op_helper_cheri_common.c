95fac6e5f56963cf070beb7e97e91a710daa91eb
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-02-24 13:12:39 +0000

    Implement CSetOffset in terms of CIncOffset
    Reduces code duplication

--- target/cheri-common/op_helper_cheri_common.c
+++ target/cheri-common/op_helper_cheri_common.c
@@ -5,5 +5,12 @@
         (deprecated("Do not call the helper directly, it will crash at "       \
                     "runtime. Call the _impl variant instead"))) helper_##name
 {
+}
+{
                                         target_ulong rs))
                        "Unknown permission bits set!");
+                                  target_ulong rt))
+void CHERI_HELPER_IMPL(candaddr(CPUArchState *env, uint32_t cd, uint32_t cb,
+    target_ulong cursor = get_capreg_cursor(env, cb);
+    target_ulong target_addr = cursor & rt;
+    cincoffset_impl(env, cd, cb, diff, GETPC(), OOB_INFO(csetoffset));
