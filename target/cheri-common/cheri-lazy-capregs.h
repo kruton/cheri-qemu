f262bfbdd2d0254557118f8756c9b048741a2e3e
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-03-20 15:44:57 +0000

    Factor out duplicated code into a helper

--- target/cheri-common/cheri-lazy-capregs.h
+++ target/cheri-common/cheri-lazy-capregs.h
@@ -2,7 +2,9 @@
     }
 }
 static inline void sanity_check_capreg(GPCapRegs *gpcrs, unsigned regnum)
+{
 #ifdef CONFIG_DEBUG_TCG
+    }
 #endif // CONFIG_DEBUG_TCG
         cheri_debug_assert(new_state == CREG_FULLY_DECOMPRESSED &&
                            "NULL/scratch is always fully decompressed");
@@ -12,7 +14,11 @@ static inline void sanity_check_capreg(GPCapRegs *gpcrs, unsigned regnum)
         return result;
         sanity_check_capreg(gpcrs, regnum);
 #endif
+#endif
+static inline void rvfi_changed_capreg(CPUArchState *env, unsigned regnum,
+                                       target_ulong cursor)
 #if defined(TARGET_RISCV) && defined(CONFIG_RVFI_DII)
+    env->rvfi_dii_trace.INTEGER.rvfi_rd_addr = regnum;
     env->rvfi_dii_trace.INTEGER.rvfi_rd_wdata = cursor;
                                  const cap_register_t *newval)
     case CREG_INTEGER:
