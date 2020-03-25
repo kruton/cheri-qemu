9acad7f43e665d168f6bcc858c760591776e3dda
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-03-25 16:31:27 +0000

    Explain that _howmuch_out_of_bounds does not trap
    Using the comment suggested by @nwf in https://github.com/CTSRD-CHERI/qemu/pull/84

--- target/cheri-common/cheri-bounds-stats.h
+++ target/cheri-common/cheri-bounds-stats.h
@@ -1,1 +1,4 @@
         do_raise_exception(env, EXCP_DEBUG, retpc);
+// Note: despite taking both CPUArchState and retpc, this function does not throw CPU
+// exceptions. The retpc parameter is there purely for diagnostic reporting.
+ static inline int64_t _howmuch_out_of_bounds(CPUArchState *env,
