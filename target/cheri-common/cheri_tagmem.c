74d748974a0453d65d57808858658492ff114419
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-03-30 15:01:28 +0100

    Drop likely/unlikely hints for full addrspace PCC
    I couldn't measure any noticeable performance difference so it probably
    doesn't matter. Moreover, these hints depends on the workload so let's just
    let the CPU branch predictor decide.

--- target/cheri-common/cheri_tagmem.c
+++ target/cheri-common/cheri_tagmem.c
@@ -1,3 +1,7 @@
+#include "cheri_tagmem.h"
+#include "cheri-helper-utils.h"
+// XXX: use hbitmap? Or a different data structure?
+#include "qemu/bitmap.h"
 #endif
  *
  * FIXME: rewrite using somethign more like the upcoming MTE changes (https://github.com/rth7680/qemu/commits/tgt-arm-mte-user)
