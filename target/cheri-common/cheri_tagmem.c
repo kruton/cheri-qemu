14f8b10ecc3aeb08ed2a865300873eb2f98800c8
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-03-31 12:58:43 +0100

    Output a warning if MTTCG is enabled
    There is no atomicity between memory writes and tag writes so enabling
    MTTCG can result in corrupted capabilities being created.

--- target/cheri-common/cheri_tagmem.c
+++ target/cheri-common/cheri_tagmem.c
@@ -7,6 +7,12 @@
  * FIXME: rewrite using somethign more like the upcoming MTE changes (https://github.com/rth7680/qemu/commits/tgt-arm-mte-user)
 #define CAP_TAGBLK_MSK      ((1 << CAP_TAGBLK_SHFT) - 1)
 #define CAP_TAGBLK_SIZE       (1 << CAP_TAGBLK_SHFT)
+static inline size_t num_tagblocks(RAMBlock* ram)
+{
+    uint64_t memory_size = memory_region_size(ram->mr);
+    size_t result = DIV_ROUND_UP(memory_size, CHERI_CAP_SIZE * CAP_TAGBLK_SIZE);
+    }
+    return result;
     assert(memory_region_is_ram(mr));
     assert(memory_region_size(mr) == memory_size &&
            "Incorrect tag mem size passed?");
