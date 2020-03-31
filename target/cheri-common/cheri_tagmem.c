6d0ec8a5c6c05e942c1186ed5dee1df74d2882ab
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-03-31 14:27:17 +0100

    Use accessor functions for cheri tag memory blocks. NFC
    This will allow comparing one byte per tag with one bit per tag storage.

--- target/cheri-common/cheri_tagmem.c
+++ target/cheri-common/cheri_tagmem.c
@@ -3,18 +3,50 @@
 // XXX: use hbitmap? Or a different data structure?
 #include "qemu/bitmap.h"
 #endif
+#endif
  *
  * FIXME: rewrite using somethign more like the upcoming MTE changes (https://github.com/rth7680/qemu/commits/tgt-arm-mte-user)
 #define CAP_TAGBLK_MSK      ((1 << CAP_TAGBLK_SHFT) - 1)
 #define CAP_TAGBLK_SIZE       (1 << CAP_TAGBLK_SHFT)
+#else
 static inline size_t num_tagblocks(RAMBlock* ram)
 {
     uint64_t memory_size = memory_region_size(ram->mr);
     size_t result = DIV_ROUND_UP(memory_size, CHERI_CAP_SIZE * CAP_TAGBLK_SIZE);
     }
     return result;
+}
+static CheriTagBlock *cheri_tag_new_tagblk(RAMBlock *ram, uint64_t tagidx)
+{
+    CheriTagBlock *tagblk, *old;
+    tagblk = g_malloc0(sizeof(CheriTagBlock));
+    if (tagblk == NULL) {
+        error_report("Can't allocate tag block.");
+        exit(1);
+    CheriTagBlock **tagmem = (CheriTagBlock **)ram->cheri_tags;
+    size_t tagblock_index = (tagidx >> CAP_TAGBLK_SHFT);
+    /* Possible race here so use atomic compare and swap. */
+    cheri_debug_assert(tagblock_index < num_tagblocks(ram) &&
+                       "Tag index out of bounds");
+    old = qatomic_cmpxchg(&tagmem[tagblock_index], NULL, tagblk);
+    if (old != NULL) {
+        /* Lost the race, free. */
+        g_free(tagblk);
+        return old;
+    } else {
+        return tagblk;
+static inline QEMU_ALWAYS_INLINE CheriTagBlock *cheri_tag_block(size_t tag_index,
+                                                                RAMBlock *ram)
+    const size_t tagbock_index = tag_index >> CAP_TAGBLK_SHFT;
+    cheri_debug_assert(ram->cheri_tags);
+    return tagmem[tagbock_index];
+static inline QEMU_ALWAYS_INLINE bool tagblock_get_tag(CheriTagBlock *block,
+                                                       size_t block_index)
+static inline QEMU_ALWAYS_INLINE void
+static inline QEMU_ALWAYS_INLINE void tagblock_clear_tag(CheriTagBlock *block,
     assert(memory_region_is_ram(mr));
     assert(memory_region_size(mr) == memory_size &&
            "Incorrect tag mem size passed?");
+    CheriTagBlock *tagblk = cheri_tag_block(tag, ram);
     if (unlikely((first_addr & TARGET_PAGE_MASK) !=
         return NULL;
