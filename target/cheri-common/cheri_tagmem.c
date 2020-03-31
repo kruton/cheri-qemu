acfa4035201ac41548302ab9bbd5c7d7e549d269
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-03-31 12:37:28 +0100

    Use a CheriTagBlock structure instead of uint8_t. NFC

--- target/cheri-common/cheri_tagmem.c
+++ target/cheri-common/cheri_tagmem.c
@@ -5,6 +5,8 @@
 #endif
  *
  * FIXME: rewrite using somethign more like the upcoming MTE changes (https://github.com/rth7680/qemu/commits/tgt-arm-mte-user)
+#define CAP_TAGBLK_MSK      ((1 << CAP_TAGBLK_SHFT) - 1)
+#define CAP_TAGBLK_SIZE       (1 << CAP_TAGBLK_SHFT)
     assert(memory_region_is_ram(mr));
     assert(memory_region_size(mr) == memory_size &&
            "Incorrect tag mem size passed?");
