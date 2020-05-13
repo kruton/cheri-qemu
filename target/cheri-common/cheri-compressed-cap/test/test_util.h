c8a4989b92aa45e887a0ed5fb5dc6f6427920a71
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-05-13 10:15:21 +0100

    Update cheri-compressed-cap
    This fixes a decoding corner case where we were disagreeing with sail.
    Fortunately, it is unlikely to happen in practise since it requires
    capabilities that are almost full-address-space.
    
    git subrepo pull target/cheri-common/cheri-compressed-cap
    
    subrepo:
      subdir:   "target/cheri-common/cheri-compressed-cap"
      merged:   "8f39bde1b4"
    upstream:
      origin:   "ctsrd:cheri-compressed-cap"
      branch:   "master"
      commit:   "b8f1a7281e"
    git-subrepo:
      version:  "0.4.1"
      origin:   "https://github.com/ingydotnet/git-subrepo.git"
      commit:   "f223adb"

---
+++ target/cheri-common/cheri-compressed-cap/test/test_util.h
@@ -0,0 +1,7 @@
+    }
+std::ostream& operator<<(std::ostream& os, const _cc_bounds_bits& value);
+std::ostream& operator<<(std::ostream& os, const _cc_bounds_bits& value) {
+    os << "{ B: " << (unsigned)value.B << " T: " << (unsigned)value.T << " E: " << (unsigned)value.E
+       << " IE: " << (unsigned)value.IE << " }";
+    return os;
+std::ostream& operator<<(std::ostream& os, const _cc_cap_t& value);
