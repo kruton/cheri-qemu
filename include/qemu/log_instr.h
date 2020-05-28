991731eea6c181d6499dfa6be61b0c7355785e4e
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2020-05-28 15:38:17 +0100

    Cleanup instruction logging subsystem.
    Remove CPU_LOG_USER_ONLY as it is now unused.
    Remove unused macros.
    Improve naming of per-CPU log levels.

--- include/qemu/log_instr.h
+++ include/qemu/log_instr.h
@@ -2,3 +2,6 @@
  * Cambridge Computer Laboratory (Department of Computer Science and
  * Technology) under DARPA contract HR0011-18-C-0016 ("ECATS"), as part of the
  * DARPA SSITH research programme.
+    QEMU_LOG_INSTR_LOGLEVEL_NONE = 0,
+    QEMU_LOG_INSTR_LOGLEVEL_ALL = 1,
+    QEMU_LOG_INSTR_LOGLEVEL_USER = 2,
