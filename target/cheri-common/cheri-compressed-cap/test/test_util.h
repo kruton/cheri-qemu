72fb858bed3a8fdbf729dd2278ead3b73e8787b9
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-05-28 11:39:01 +0100

    git subrepo commit target/cheri-common/cheri-compressed-cap
    subrepo:
      subdir:   "target/cheri-common/cheri-compressed-cap"
      merged:   "38f27dab80"
    upstream:
      origin:   "ctsrd:cheri-compressed-cap"
      branch:   "master"
      commit:   "5518706b18"
    git-subrepo:
      version:  "0.4.1"
      origin:   "https://github.com/ingydotnet/git-subrepo.git"
      commit:   "f223adb"

--- target/cheri-common/cheri-compressed-cap/test/test_util.h
+++ target/cheri-common/cheri-compressed-cap/test/test_util.h
@@ -1,7 +1,10 @@
     }
+}
 std::ostream& operator<<(std::ostream& os, const _cc_bounds_bits& value);
 std::ostream& operator<<(std::ostream& os, const _cc_bounds_bits& value) {
     os << "{ B: " << (unsigned)value.B << " T: " << (unsigned)value.T << " E: " << (unsigned)value.E
        << " IE: " << (unsigned)value.IE << " }";
     return os;
 std::ostream& operator<<(std::ostream& os, const _cc_cap_t& value);
+std::ostream& operator<<(std::ostream& os, const _cc_cap_t& value) {
+std::ostream& operator<<(std::ostream& os, const cc128_length_t& value);
