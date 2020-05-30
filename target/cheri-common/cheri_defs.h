e8d0a975a62a99e822d229450bba3c9bda348c52
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-05-30 17:20:39 +0100

    Fix fatal error introduced in 840bde4d1d0cc73df39a653d228acfc46d2b47fe
    This resulted in the magic128 MAX_TOP value being used instead of the
    real value of 2^64 and caused an assertion when calling
    "csetbounds cap, UINT64_MAX". This happens early in the CheriBSD boot so
    we couldn't run CheriBSD anymore.

--- target/cheri-common/cheri_defs.h
+++ target/cheri-common/cheri_defs.h
@@ -1,3 +1,6 @@
+#else
 #define CAP_UPERMS_SHFT CAP_CC(UPERMS_SHFT)
+#define CAP_MAX_LENGTH CAP_CC(MAX_LENGTH)
+#define CAP_MAX_TOP CAP_CC(MAX_TOP)
     /* DDC is tagged, unsealed and cursor (addresss) is zero */
     /* DDC is tagged, unsealed and top is max_addr  */
