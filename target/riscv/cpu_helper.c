7dd6fb9d5427230098ec884baa913b04f27c43a4
Nathaniel Filardo <nfilardo@microsoft.com>
2020-10-22 14:11:46 +0100

    CHERI riscv: PTE load-side caveats for non-LCG behaviors

--- target/riscv/cpu_helper.c
+++ target/riscv/cpu_helper.c
@@ -1604,9 +1604,14 @@ static int get_physical_address(CPURISCVState *env, hwaddr *physical,
         if ((i != (levels - 1)) || (napot_bits != 4)) {
             return TRANSLATE_FAIL;
     }
+    }
         (access_type == MMU_DATA_STORE ||
          access_type == MMU_DATA_CAP_STORE || (pte & PTE_D))) {
     if ((pte & PTE_CR) == 0) {
+        if ((pte & PTE_CRM) == 0) {
+            prot |= PAGE_LC_CLEAR;
+        } else {
+            prot |= PAGE_LC_TRAP;
         prot |= PAGE_SC_TRAP;
         if (!(pte & PTE_CW)) {
         if (!(pte & PTE_CW)) {
