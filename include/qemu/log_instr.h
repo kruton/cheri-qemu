338700a71dfe8772c8690a5e85b7fd68b9e14438
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2020-10-28 17:40:20 +0000

    Only dump new instructions when flushing the instruction log buffer multiple times.

--- include/qemu/log_instr.h
+++ include/qemu/log_instr.h
@@ -5,3 +5,6 @@
     QEMU_LOG_INSTR_LOGLEVEL_NONE = 0,
     QEMU_LOG_INSTR_LOGLEVEL_ALL = 1,
     QEMU_LOG_INSTR_LOGLEVEL_USER = 2,
+    /* Ring buffer index of the next entry to write */
+    /* Ring buffer index of the first entry to dump */
+    size_t ring_tail;
