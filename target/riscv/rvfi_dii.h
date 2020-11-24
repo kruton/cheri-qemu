b375e23e29faca3bb2043d60a41ddd5128b88b26
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-11-24 11:01:26 +0000

    RVFI-DII: fix memory data packet layout
    The address is the last field and not the first one.

---
+++ target/riscv/rvfi_dii.h
@@ -0,0 +1,3 @@
+    //// For memory operations (rvfi_mem_rmask and/or rvfi_mem_wmask are
+    /// non-zero), rvfi_mem_addr holds the accessed memory location.
+    uint64_t rvfi_mem_addr; // output [NRET * XLEN   - 1 : 0] rvfi_mem_addr
