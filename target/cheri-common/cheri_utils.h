91cb1e743be01d2aad05ab46c69fcfff0ce1126f
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-11-30 14:54:54 +0000

    Match sail for cgetlen when top < base
    This was changed in 8a78f333e042a9e8c5fda7d749918aa78ea1da61 but I don't
    recall why that change was made. It conflicts with the sail behaviour
    and does not make much sense (since they length is unsigned), so revert it.
    This case can only happen for invalid capabilities, where cgetlen is of
    questionable usefulness, so this should not result in noticeable behaviour
    changes.

--- target/cheri-common/cheri_utils.h
+++ target/cheri-common/cheri_utils.h
@@ -1,1 +1,5 @@
+}
     // TODO: should handle last byte of address space properly
+static inline cap_length_t cap_get_length_full(const cap_register_t *c)
+    cheri_debug_assert((!c->cr_tag || c->_cr_top >= c->cr_base) &&
+                       "Tagged capabilities must be in bounds!");
