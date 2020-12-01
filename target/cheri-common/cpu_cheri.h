b109cb42919555359b9cf482af472c1f57964aeb
Lawrence Esswood <le277@cam.ac.uk>
2020-12-01 14:03:13 +0000

    Make DDC bounds checker into a more general DDC/PCC bounds checker
    Co-authored-by: Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>

--- target/cheri-common/cpu_cheri.h
+++ target/cheri-common/cpu_cheri.h
@@ -1,8 +1,10 @@
     cheri_debug_assert(!pcc->cr_tag || cap_is_unsealed(pcc) ||
                        pcc->_cr_cursor == pc_addr);
+        *cheri_flags |= TB_FLAG_CHERI_PCC_EXECUTABLE;
     if (pcc->cr_tag && cap_is_unsealed(pcc)) {
         if (cap_has_perms(pcc, CAP_PERM_LOAD))
             *cheri_flags |= TB_FLAG_CHERI_PCC_READABLE;
         *cheri_flags |= TB_FLAG_CHERI_PCC_TOP_MAX;
+        if (cap_has_perms(ddc, CAP_PERM_LOAD))
         if (cap_has_perms(ddc, CAP_PERM_STORE))
             *cheri_flags |= TB_FLAG_CHERI_DDC_WRITABLE;
