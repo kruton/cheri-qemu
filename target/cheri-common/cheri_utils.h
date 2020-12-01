db9754455c8dfad36b4e35a3178380c5d8cf88a9
Lawrence Esswood <le277@cam.ac.uk>
2020-12-01 14:02:03 +0000

    Fix otype sign related issues for AARCH64

--- target/cheri-common/cheri_utils.h
+++ target/cheri-common/cheri_utils.h
@@ -1,3 +1,6 @@
+ */
+#ifdef TARGET_AARCH64
+#else
 static inline target_ulong cap_get_cursor(const cap_register_t *c)
 {
 }
@@ -5,10 +8,12 @@ static inline target_ulong cap_get_base(const cap_register_t *c)
 {
 }
 static inline cap_offset_t cap_get_offset(const cap_register_t *c)
+/*
     // TODO: should handle last byte of address space properly
 static inline cap_length_t cap_get_length_full(const cap_register_t *c)
     cheri_debug_assert((!c->cr_tag || c->_cr_top >= c->cr_base) &&
                        "Tagged capabilities must be in bounds!");
+        return result;
 static inline void cap_set_cursor(cap_register_t *cap, uint64_t new_addr)
     if (!is_representable_cap_with_addr(cap, new_addr)) {
         cap_mark_unrepresentable(new_addr, cap);
