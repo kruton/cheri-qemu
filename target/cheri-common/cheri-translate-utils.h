54ec0ecd3bd1dbb619b1819fbcbc58339f1dca61
Lawrence Esswood <le277@cam.ac.uk>
2020-12-01 14:03:32 +0000

    Add TCG implementations of some capability operations

--- target/cheri-common/cheri-translate-utils.h
+++ target/cheri-common/cheri-translate-utils.h
@@ -1,5 +1,18 @@
 }
+// TODO Move to target specific headers
+#define target_get_gpr(ctx, t, reg) gen_load_gpr((TCGv)t, reg)
+#define MERGED_FILE 0
 #elif defined(TARGET_AARCH64)
+#define target_get_gpr(ctx, t, reg)                                            \
+    if (reg == NULL_CAPREG_INDEX)                                              \
+        tcg_gen_movi_tl((TCGv)t, 0);                                           \
+    else                                                                       \
+        tcg_gen_mov_i64((TCGv)t, cpu_reg_sp(ctx, reg))
+#define MERGED_FILE 1
+#elif defined(TARGET_RISCV)
+#define target_get_gpr(ctx, t, reg) gen_get_gpr(ctx, (TCGv)t, reg)
+#else
+#error "Don't know how to fetch a GPR value"
         return;
     if (unlikely(do_checks)) {
     if (have_cheri_tb_flags(ctx, TB_FLAG_CHERI_PCC_FULL_AS)) {
