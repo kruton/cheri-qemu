50da7bdbc0db4841f1d20511b4b36864790021e4
Lawrence Esswood <le277@cam.ac.uk>
2021-01-25 16:39:14 +0000

    Make Morello specific changes to cheri utils

--- target/cheri-common/cheri_utils.h
+++ target/cheri-common/cheri_utils.h
@@ -1,6 +1,7 @@
  */
 #ifdef TARGET_AARCH64
 #else
+#endif
 static inline target_ulong cap_get_cursor(const cap_register_t *c)
 {
 }
@@ -9,6 +10,7 @@ static inline target_ulong cap_get_base(const cap_register_t *c)
 }
 static inline cap_offset_t cap_get_offset(const cap_register_t *c)
 /*
+#ifndef TARGET_AARCH64
     // TODO: should handle last byte of address space properly
 static inline cap_length_t cap_get_length_full(const cap_register_t *c)
     cheri_debug_assert((!c->cr_tag || c->_cr_top >= c->cr_base) &&
