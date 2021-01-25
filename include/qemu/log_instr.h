a231fd2844db8105a428eba5602cf4264e0eb2e8
Lawrence Esswood <le277@cam.ac.uk>
2021-01-25 16:15:47 +0000

    Add printf-like instruction logging function
    Unlike qemu_log_instr_extra(), this can be called while translating and
    avoids having to write a separate helper for every formatted instruction
    tracing output.

--- include/qemu/log_instr.h
+++ include/qemu/log_instr.h
@@ -1,3 +1,4 @@
+/*
  * Copyright (c) 2020 Alfredo Mazzinghi
  * Cambridge Computer Laboratory (Department of Computer Science and
  * Technology) under DARPA contract HR0011-18-C-0016 ("ECATS"), as part of the
