a650ccaf3dc88f9c048cb161ee2623efb6b5a407
Lawrence Esswood <le277@cam.ac.uk>
2021-01-25 16:35:11 +0000

    Give CapRegState a to string helper

--- target/cheri-common/cheri-lazy-capregs-types.h
+++ target/cheri-common/cheri-lazy-capregs-types.h
@@ -2,3 +2,9 @@
     CREG_UNTAGGED_CAP = 0b01,
     CREG_TAGGED_CAP = 0b10,
     CREG_FULLY_DECOMPRESSED = 0b11,
+static inline const char *cap_reg_state_string(CapRegState state)
+{
+    const char *strings[] = {"Int", "Untagged Cap", "Tagged Cap",
+                             "Decompressed"};
+    return strings[(int)state];
+}
