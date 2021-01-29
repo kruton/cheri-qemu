7c809872e54dbcb6cf8bf33b0234fc2d29b970be
Lawrence Esswood <le277@cam.ac.uk>
2021-01-29 17:21:00 +0000

    Better tracing of changes to registers

--- target/cheri-common/cheri-translate-utils.h
+++ target/cheri-common/cheri-translate-utils.h
@@ -1,4 +1,6 @@
 }
+    }
+#endif
 // TODO Move to target specific headers
 #define target_get_gpr(ctx, t, reg) gen_load_gpr((TCGv)t, reg)
 #define MERGED_FILE 0
@@ -13,8 +15,10 @@
 #define target_get_gpr(ctx, t, reg) gen_get_gpr(ctx, (TCGv)t, reg)
 #else
 #error "Don't know how to fetch a GPR value"
+#else
         return;
     if (unlikely(do_checks)) {
+#ifdef TARGET_AARCH64
     if (have_cheri_tb_flags(ctx, TB_FLAG_CHERI_PCC_FULL_AS)) {
     // Note: JR/JALR will often be used in hybrid/non-CHERI cases, so we can
     // skip the less than check if pcc.base is zero and top is MAX:
@@ -23,5 +27,16 @@
         tcg_gen_brcondi_tl(TCG_COND_LTU, addr, ctx->base.pcc_base,
                            bounds_violation);
         tcg_gen_brcondi_tl(TCG_COND_GEU, addr, ctx->base.pcc_top,
+    if (qemu_ctx_logging_enabled(ctx)) {
+        TCGv_ptr name = tcg_constant_ptr(str_name);
+        gen_helper_qemu_log_instr_cap(tcg_env, name, reg,
+        gen_ensure_cap_decompressed(ctx, regnum);
+        gen_helper_qemu_log_instr_reg(tcg_env, name, new_val,
+        TCGv new_val = tcg_temp_new();
+        gen_cap_get_cursor(ctx, regnum, new_val);
+        const char *str_name =
+            cheri_gp_int_regnames[regnum];
+            cheri_gp_regnames[regnum];
+        // TODO: Add some integer names to riscv/mips
         return;
     if (regnum == NULL_CAPREG_INDEX)
