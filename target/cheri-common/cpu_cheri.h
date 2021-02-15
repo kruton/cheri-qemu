b6aa2a19e0f70c094c89df2e91776d07318fbc18
Lawrence Esswood <le277@cam.ac.uk>
2021-02-15 18:52:57 +0000

    TCG for bounds checking normal loads and stores

--- target/cheri-common/cpu_cheri.h
+++ target/cheri-common/cpu_cheri.h
@@ -1,3 +1,4 @@
+}
     cheri_debug_assert(!pcc->cr_tag || cap_is_unsealed(pcc) ||
                        pcc->_cr_cursor == pc_addr);
     cheri_debug_assert(*cheri_flags == 0);
@@ -7,8 +8,12 @@
     if (pcc->cr_tag && cap_is_unsealed(pcc)) {
         if (cap_has_perms(pcc, CAP_PERM_LOAD))
             *cheri_flags |= TB_FLAG_CHERI_PCC_READABLE;
+    if (*pcc_base == 0)
+        *cheri_flags |= TB_FLAG_CHERI_PCC_BASE_ZERO;
+    if (cap_get_top_full(pcc) == CAP_MAX_TOP)
         *cheri_flags |= TB_FLAG_CHERI_PCC_TOP_MAX;
         if (cap_has_perms(ddc, CAP_PERM_LOAD))
             *cheri_flags |= TB_FLAG_CHERI_DDC_READABLE;
         if (cap_has_perms(ddc, CAP_PERM_STORE))
             *cheri_flags |= TB_FLAG_CHERI_DDC_WRITABLE;
+            *cheri_flags |= TB_FLAG_CHERI_DDC_TOP_MAX;
