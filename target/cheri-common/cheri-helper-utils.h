415a2142af249bd41c0297d8122e892ba9fba929
Lawrence Esswood <le277@cam.ac.uk>
2021-03-31 13:29:46 +0100

    Do not require storecap permission if storing untagged values
    This matches the ISA spec and the sail code for MIPS and RISC-V, but was
    previously being checked even for untagged values.

--- target/cheri-common/cheri-helper-utils.h
+++ target/cheri-common/cheri-helper-utils.h
@@ -2,3 +2,6 @@
     // we only really care about the bounds and permissions to derive the
     // resulting capability (e.g. auipcc or CGetPCCIncOffset).
     // If the result is not representable it will be untagged.
+    uint32_t perms = CAP_PERM_STORE;
+    if (get_capreg_tag(env, cs))
+        perms |= CAP_PERM_STORE_CAP;
