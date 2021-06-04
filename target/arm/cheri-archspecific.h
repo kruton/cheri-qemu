841bed7b6f041c4e60ecde242dc969ee9e37b281
Lawrence Esswood <le277@cam.ac.uk>
2021-06-04 12:22:19 +0100

    Restore state before CHERI exceptions on morello

--- target/arm/cheri-archspecific.h
+++ target/arm/cheri-archspecific.h
@@ -1,1 +1,7 @@
 #define CAP_TAG_GET_MANY_SHFT 2
+    }
+    if (hostpc) {
+        // AARCH's cpu_restore_state will reset syndrome, so don't use
+        // raise_exception_ra here.
+        CPUState *cs = env_cpu(env);
+        cpu_restore_state(cs, hostpc);
