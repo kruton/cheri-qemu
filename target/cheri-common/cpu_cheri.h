df14b900fba4dbb3183f70d079d2669d5980981c
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2021-06-09 20:53:55 +0100

    Avoid ternary expressions in cheri_flags compuatation
    This makes it a bit more readable but should generate the same code.

--- target/cheri-common/cpu_cheri.h
+++ target/cheri-common/cpu_cheri.h
@@ -1,10 +1,14 @@
     cheri_debug_assert(!pcc->cr_tag || cap_is_unsealed(pcc) ||
                        pcc->_cr_cursor == pc_addr);
+    cheri_debug_assert(*cheri_flags == 0);
+        *cheri_flags |= TB_FLAG_CHERI_CAPMODE;
+    if (cheri_cap_perms_valid_for_exec(pcc))
         *cheri_flags |= TB_FLAG_CHERI_PCC_EXECUTABLE;
     if (pcc->cr_tag && cap_is_unsealed(pcc)) {
         if (cap_has_perms(pcc, CAP_PERM_LOAD))
             *cheri_flags |= TB_FLAG_CHERI_PCC_READABLE;
         *cheri_flags |= TB_FLAG_CHERI_PCC_TOP_MAX;
         if (cap_has_perms(ddc, CAP_PERM_LOAD))
+            *cheri_flags |= TB_FLAG_CHERI_DDC_READABLE;
         if (cap_has_perms(ddc, CAP_PERM_STORE))
             *cheri_flags |= TB_FLAG_CHERI_DDC_WRITABLE;
