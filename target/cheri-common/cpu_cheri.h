147884a38a2c415f6435de6761309e064e46deb9
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2021-06-09 14:05:11 +0100

    Rename TB_FLAG_PCC_FULL_AS to TB_FLAG_CHERI_PCC_FULL_AS
    The previous name was inconsistent.

--- target/cheri-common/cpu_cheri.h
+++ target/cheri-common/cpu_cheri.h
@@ -1,3 +1,4 @@
     cheri_debug_assert(!pcc->cr_tag || cap_is_unsealed(pcc) ||
                        pcc->_cr_cursor == pc_addr);
     if (pcc->cr_tag && cap_is_unsealed(pcc)) {
+        *cheri_flags |= TB_FLAG_CHERI_PCC_TOP_MAX;
