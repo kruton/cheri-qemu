995c21d98948784b248ebedc0400a8ca75a5e934
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2021-06-11 16:32:22 +0100

    [Morello] Implement -cheri-debugger-on-unrepresentable

--- target/cheri-common/cheri-bounds-stats.h
+++ target/cheri-common/cheri-bounds-stats.h
@@ -1,4 +1,7 @@
+    if (cheri_debugger_on_unrepresentable)
         do_raise_exception(env, EXCP_DEBUG, retpc);
+#elif defined(TARGET_AARCH64)
+        HELPER(exception_internal)(env, EXCP_DEBUG);
 // Note: despite taking both CPUArchState and retpc, this function does not throw CPU
 // exceptions. The retpc parameter is there purely for diagnostic reporting.
  static inline int64_t _howmuch_out_of_bounds(CPUArchState *env,
