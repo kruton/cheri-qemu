0c0f4ac86def4bffd0f2e96a10e063f161b15ae4
Lawrence Esswood <le277@cam.ac.uk>
2021-07-07 12:46:18 +0100

    Fix comments and whitespace

--- tcg/tcg.c
+++ tcg/tcg.c
@@ -6857,6 +6857,7 @@ static void sync_global(TCGContext *s, TCGOp *op)
     tcg_debug_assert(ts->val_type == TEMP_VAL_REG ||
                      ts->val_type == TEMP_VAL_MEM);
     // Liveness analysis should make sure that the sync happened at the last
+    // write
     tcg_debug_assert((ts->val_type != TEMP_VAL_REG) || (ts->mem_coherent));
 }
 int tcg_gen_code(TCGContext *s, TranslationBlock *tb, uint64_t pc_start)
