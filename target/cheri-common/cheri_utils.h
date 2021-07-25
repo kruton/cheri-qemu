48970aeca0c1565cb5a606a88ce427c22ffd3ca2
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2021-07-25 07:32:20 +0100

    [NFC] Clang-format the capability accessor helpers

--- target/cheri-common/cheri_utils.h
+++ target/cheri-common/cheri_utils.h
@@ -1,4 +1,8 @@
+static inline target_ulong cap_get_cursor(const cap_register_t *c)
+{
 }
+static inline target_ulong cap_get_base(const cap_register_t *c)
+static inline cap_offset_t cap_get_offset(const cap_register_t *c)
     // TODO: should handle last byte of address space properly
 static inline cap_length_t cap_get_length_full(const cap_register_t *c)
     cheri_debug_assert((!c->cr_tag || c->_cr_top >= c->cr_base) &&
