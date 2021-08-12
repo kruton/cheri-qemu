a59f81d5b04744a11fec49ef4efd2803e8ca59aa
Lawrence Esswood <le277@cam.ac.uk>
2021-08-12 15:38:39 +0100

    Generate correct ESR for MMU faults due to tags

--- target/arm/cheri-archspecific.h
+++ target/arm/cheri-archspecific.h
@@ -5,3 +5,4 @@
         // raise_exception_ra here.
         CPUState *cs = env_cpu(env);
         cpu_restore_state(cs, hostpc);
+    raise_cheri_exception_impl_if_wnr(env, CapEx_TLBNoStoreCap, cb, va, false,
