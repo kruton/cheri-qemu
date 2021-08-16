cc03ac46d3fefb6eadaf88e4a471c23ca1b28f97
Lawrence Esswood <le277@cam.ac.uk>
2021-08-16 18:07:02 +0100

    Sort fault priority of atomic cap operations

--- target/arm/cheri_helper.c
+++ target/arm/cheri_helper.c
@@ -1,5 +1,11 @@
+                        _host_return_address);
+    // Now do a probe early, so that we get the store fault with priority
+    // (LC/SC MMU fault priority) is the reverse of cap permissions)
     bool cd_tagged = get_without_decompress_tag(env, cd);
     int mmu_index = cpu_mmu_index(env_cpu(env), false);
     if (cd_tagged)
+        probe_cap_write(env, addr, CHERI_CAP_SIZE, mmu_index,
     } else {
+        cd_tagged = get_without_decompress_tag(env, cd);
         // Even if there is no store, we possibly need an MMU permission fault
+        if (!cd_tagged)
