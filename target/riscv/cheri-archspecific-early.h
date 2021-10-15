ae0e681882d84a6d9de02e24acf5eac8e128e045
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2021-10-15 10:42:56 +0100

    Move CapRegState into a new cap_register_t field
    This allows us to simply copy cap_register_t values and not have to worry
    about updating the external capreg_state array.
    Longer-term it would be good to have cheri-compressed-cap transparently
    handle the bounds decoded/raw bits states, but for now it is a lot easier
    to add a cr_extra field to retain the existing meaning of the CREG_*
    constants. This also means we don't have to adjust the Morello TCG code
    that uses these constants.

--- target/riscv/cheri-archspecific-early.h
+++ target/riscv/cheri-archspecific-early.h
@@ -1,4 +1,5 @@
  * This controls how many tags are fetched with cgettags/csettags
 #define CAP_TAG_GET_MANY_SHFT 3
 #define CINVOKE_DATA_REGNUM 31
+    cheri_debug_assert(env->ddc.cr_extra == CREG_FULLY_DECOMPRESSED);
 #define CHERI_GPCAPREGS_MEMBER gpcapregs
