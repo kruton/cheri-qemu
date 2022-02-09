5c75aa62769cdac4759eea75a7aec2fd12195864
Jessica Clarke <jrtc27@jrtc27.com>
2022-02-09 21:16:16 +0000

    Don't warn about untagged capabilities that wrap

--- target/cheri-common/cheri_utils.h
+++ target/cheri-common/cheri_utils.h
@@ -49,6 +49,10 @@ static inline void cap_unseal_reserved_otype(cap_register_t *c)
 #ifdef TARGET_AARCH64
     // Invalid exponent caps are always considered out of bounds.
     if (!c->cr_bounds_valid)
+        if (c->cr_tag)
+            warn_report("Found capability access that wraps around: 0x" TARGET_FMT_lx
+                        " + %zd. Authorizing cap: " PRINT_CAP_FMTSTR,
+                        addr, num_bytes, PRINT_CAP_ARGS(c));
 static inline void cap_set_cursor(cap_register_t *cap, uint64_t new_addr)
     if (!is_representable_cap_with_addr(cap, new_addr)) {
         cap_mark_unrepresentable(new_addr, cap);
