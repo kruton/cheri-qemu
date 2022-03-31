1c8d10d938c6765e1a3419801203f10b6cee74ea
Alexander Richardson <Alexander.Richardson@cl.cam.ac.uk>
2022-03-31 12:40:12 +0100

    [Morello] Don't assert jump targets are representable (#195)
    Morello checks the bounds at the jump target, so it is possible that the
    target PCC is unrepresentable after a jump. I believe this should fix
    a "Target addr must be representable" assertion that I saw a while ago.

--- target/riscv/cheri-archspecific-early.h
+++ target/riscv/cheri-archspecific-early.h
@@ -1,5 +1,6 @@
  * This controls how many tags are fetched with cgettags/csettags
 #define CAP_TAG_GET_MANY_SHFT 3
+#define CHERI_CONTROLFLOW_CHECK_AT_TARGET 0
 #define CINVOKE_DATA_REGNUM 31
     cheri_debug_assert(env->ddc.cr_extra == CREG_FULLY_DECOMPRESSED);
 #define CHERI_GPCAPREGS_MEMBER gpcapregs
