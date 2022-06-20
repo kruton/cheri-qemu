56762bd8ad22a550ebd3a5abed45fa61c2e30d46
Atish Patra <atish.patra@wdc.com>
2022-06-20 16:15:55 -0700

    target/riscv: Implement mcountinhibit CSR
    commit b1675eeb3e6e38b042a23a9647559c9c548c733d upstream
    
    As per the privilege specification v1.11, mcountinhibit allows to start/stop
    a pmu counter selectively.
    
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Signed-off-by: Atish Patra <atish.patra@wdc.com>
    Signed-off-by: Atish Patra <atishp@rivosinc.com>
    Message-Id: <20220620231603.2547260-6-atishp@rivosinc.com>
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>
    [ Changed CSR function table initialisations to use macros ]
    Signed-off-by: Stuart Menefy <stuart.menefy@codasip.com>

--- target/riscv/machine.c
+++ target/riscv/machine.c
@@ -490,7 +490,6 @@ const VMStateDescription vmstate_riscv_cpu = {
         VMSTATE_UINTTL_OR_CAP(env.stvec, env.stvecc, RISCVCPU),
         VMSTATE_UINTTL_OR_CAP(env.sepc, env.sepcc, RISCVCPU),
         VMSTATE_UINTTL(env.scause, RISCVCPU),
-        VMSTATE_UINTTL(env.mtvec, RISCVCPU),
         VMSTATE_UINTTL_OR_CAP(env.mtvec, env.mtvecc, RISCVCPU),
         VMSTATE_UINTTL_OR_CAP(env.mepc, env.mepcc, RISCVCPU),
         VMSTATE_UINTTL(env.mcause, RISCVCPU),
