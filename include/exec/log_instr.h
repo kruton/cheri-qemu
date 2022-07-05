b7404c0bb828237fd43944a30dffefd1674cecf8
Jessica Clarke <jrtc27@jrtc27.com>
2022-07-05 21:02:48 +0100

    Clean up TCG instruction logging printf handling a little
    Currently we conflate qemu_log_printf_valid_entries and printf_used_ptr
    handling, but the former is a dynamic TCG variable that is relied upon
    being cleared after printing and the latter is a bit of tracking used
    during translation. Instead, clear the former as we dump in the helper
    rather than require generated code at every site to clear it. This also
    allows it to no longer be exposed outside the logging implementation.
    
    We can also do the same with printf_used_ptr and keep its handling
    hidden within the logging implementation rather than split between there
    and translation.

--- include/exec/log_instr.h
+++ include/exec/log_instr.h
@@ -1,3 +1,6 @@
+/* Should only be called in a place it cannot be skipped by a branch! */
+void qemu_log_gen_printf_flush(struct DisasContextBase *base, bool flush_early,
+                               bool force_flush);
                            const cap_register_t *value);
                         target_ulong arg1, target_ulong arg2,
                         target_ulong arg3);
