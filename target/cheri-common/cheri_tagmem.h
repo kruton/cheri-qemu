d66cb3ff97dd0cbe8278c2bcc3bfdb408f38eebd
John Baldwin <jhb@FreeBSD.org>
2022-11-15 17:42:22 -0800

    Support for fetching in-memory capabilities via GDB.
    qXfer:capa:read reads a single capability.  The annex portion of the
    command is the virtual address of the capability.  The offset and
    length fields are relative to the 17 byte returned value.  The tag is
    returned in the first byte.

--- target/cheri-common/cheri_tagmem.h
+++ target/cheri-common/cheri_tagmem.h
@@ -1,5 +1,9 @@
+ */
 #include "system/memory.h"
 void cheri_tag_init(MemoryRegion* mr, uint64_t memory_size);
+/**
 void cheri_tag_invalidate(CPUArchState *env, target_ulong vaddr, int32_t size,
 int cheri_tag_get_many(CPUArchState *env, target_ulong vaddr, int reg,
 void *cheri_tag_set(CPUArchState *env, target_ulong vaddr, int reg,
+ * Fetch a single tag for use by the debug stub.
+bool cheri_tag_get_debug(RAMBlock *ram, ram_addr_t ram_offset);
