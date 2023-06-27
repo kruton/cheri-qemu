539e1d7b9b5c07a5f7ae979df29cd4bc88cd7ce2
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2023-06-27 14:23:54 -0700

    [CHERI-RISC-V] Turn trapping semantics on again
    The latest CheriBSD release is not ready for these changes and has test
    failures due to these semantic changes. This will be turned on again in
    the future but we have to disable it for now as it prevents merges from
    QEMU dev to qemu-cheri.

--- target/riscv/cheri-archspecific-early.h
+++ target/riscv/cheri-archspecific-early.h
@@ -1,7 +1,8 @@
  * This controls how many tags are fetched with cgettags/csettags
 #define CAP_TAG_GET_MANY_SHFT 3
 #define CHERI_CONTROLFLOW_CHECK_AT_TARGET 0
-#define CHERI_TAG_CLEAR_ON_INVALID        1
+/* TODO: switch tag clearing to true once CheriBSD is ready for it. */
+#define CHERI_TAG_CLEAR_ON_INVALID 0
 #define CINVOKE_DATA_REGNUM 31
     cheri_debug_assert(env->ddc.cr_extra == CREG_FULLY_DECOMPRESSED);
 #define CHERI_GPCAPREGS_MEMBER gpcapregs
