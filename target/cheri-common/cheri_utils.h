ce28f04373a4e1ab89b5b59c853d65d9eceb6ad8
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2023-07-25 16:43:45 -0700

    Fix assertion when marking capabilities as unrepresentable
    After the latest update to cheri-compressed-cap compress_raw() asserts
    that the bounds interpretation hasn't changed, but cap_mark_unrepresentable
    updates the cursor before calling it so this invariant does not hold.
    This allows removing a rather ancient TODO item dating back to CHERI-MIPS
    when QEMU store uncompressed bounds and only computed PESBT when storing
    to memory.

--- target/cheri-common/cheri_utils.h
+++ target/cheri-common/cheri_utils.h
@@ -16,12 +16,16 @@ static inline cap_offset_t cap_get_offset(const cap_register_t *c)
 /*
  */
     /*
+     */
+#endif
 #endif
 #endif
 #endif
     return false;
+    /*
 #else
 #ifndef TARGET_AARCH64
+#else
     // TODO: should handle last byte of address space properly
 static inline cap_length_t cap_get_length_full(const cap_register_t *c)
 #ifndef TARGET_AARCH64
@@ -53,7 +57,11 @@ static inline void cap_unseal_reserved_otype(cap_register_t *c)
             warn_report("Found capability access that wraps around: 0x" TARGET_FMT_lx
                         " + %zd. Authorizing cap: " PRINT_CAP_FMTSTR,
                         addr, num_bytes, PRINT_CAP_ARGS(c));
+     * Recompute the decompressed bounds relative to the new address. In most
+     * cases they will refer to a different region of memory now.
+    CAP_cc(decompress_raw_ext)(cr->cr_pesbt, addr, false, lvbits, cr);
     cr->cr_extra = CREG_FULLY_DECOMPRESSED;
+#ifdef TARGET_AARCH64
 static inline void cap_set_cursor(cap_register_t *cap, uint64_t new_addr)
     if (!is_representable_cap_with_addr(cap, new_addr)) {
         cap_mark_unrepresentable(new_addr, cap);
