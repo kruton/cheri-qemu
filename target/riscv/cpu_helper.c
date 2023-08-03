ef9c244391d7d012fd9af7110017a6f2bb8d2ac1
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2023-08-03 08:57:30 -0700

    [CHERI-RISC-V][ISAv9] Use address instead of offset for SCRs
    When ISAv9 semantics are enabled, reading/writing an SCR should affect
    the address and not the offset of a capability.

--- target/riscv/cpu_helper.c
+++ target/riscv/cpu_helper.c
@@ -2681,3 +2681,16 @@ void riscv_cpu_do_interrupt(CPUState *cs)
 #endif /* !CONFIG_USER_ONLY */
 void update_special_register(CPURISCVState *env, cap_register_t *scr,
                              const char *name, target_ulong new_value)
+    /* The new behaviour is that SCR updates affect the address. */
+    target_ulong new_cursor = new_value;
+    if (!CHERI_NO_RELOCATION(env)) {
+        /*
+         * Legacy behaviour: with relocation enabled, updates to the SCRs update
+         * the offset since updates to the architectural PC affect offset:
+         * This shall be equivalent to a CSetOffset instruction, but with any
+         * exception condition instead just clearing the tag of the SCR.
+         */
+        target_ulong current_offset = cap_get_offset(scr);
+        target_ulong diff = new_value - current_offset;
+        new_cursor = cap_get_cursor(scr) + diff;
+    }
