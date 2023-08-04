2f4e004c447d689154df28e9d3650f1368cb1b78
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2023-08-04 15:26:09 -0700

    [CHERI-MIPS] Avoid printing executed instructions when zero
    This extra print breaks some of the unit tests, so only do it when
    non-zero.

--- target/mips/cpu.c
+++ target/mips/cpu.c
@@ -544,10 +544,18 @@ static void dump_cpu_ips_on_exit(void) {
     CPU_FOREACH(cpu) {
         CPUMIPSState *env = cpu_env(cpu);
         double duration_s = (get_clock() - start_ns) / 1000000000.0;
+        uint64_t inst_total =
             env->statcounters_icount_kernel + env->statcounters_icount_user;
+        if (inst_total == 0) {
+            continue;
         }
+        info_report("CPU%d executed instructions: %jd (%jd user, %jd kernel) "
+                    "in %.2fs KIPS: %.2f\r\n",
+                    cpu->cpu_index, (uintmax_t)inst_total,
+                    (uintmax_t)env->statcounters_icount_user,
                     (uintmax_t)env->statcounters_icount_kernel, duration_s,
                     (double)(inst_total / duration_s) / 1000.0);
+    }
 #if defined(DO_CHERI_STATISTICS)
     if (qemu_log_enabled() && qemu_loglevel_mask(CPU_LOG_INSTR | CPU_LOG_CHERI_BOUNDS)) {
         cheri_cpu_dump_statistics_f(NULL, logf, 0);
