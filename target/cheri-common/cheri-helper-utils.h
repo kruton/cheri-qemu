f49f25d6a0ae6aa9a98422e108e70337e7500cd3
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2023-08-08 10:56:52 -0700

    [CHERI] Do not exit on an infinite trap loop
    Just print an error message once instead of exiting. Killing the entire
    QEMU instance is not necessary as this could cause issues e.g. when running
    a broken VM inside QEMU (a trapping nested VM should not bring down QEMU).

--- target/cheri-common/cheri-helper-utils.h
+++ target/cheri-common/cheri-helper-utils.h
@@ -6,8 +6,15 @@
     // resulting capability (e.g. auipcc or CGetPCCIncOffset).
     // If the result is not representable it will be untagged.
     return;
+#endif
+    if (cap_exactly_equal(pcc, src_cap) && new_pc == cap_get_cursor(pcc)) {
+        if (!pcc->cr_tag || !cap_has_perms(pcc, CAP_PERM_EXECUTE) ||
+            !cap_cursor_in_bounds(pcc)) {
+            /* Warn about infinite trap loops instead of silently freezing. */
             error_report_once("Detected infinite trap loop due to invalid "
+                              "exception handler: " PRINT_CAP_FMTSTR "\r",
                               PRINT_CAP_ARGS(pcc));
+                     PRINT_CAP_ARGS(pcc));
     /*
      * Morello does not require ASR when executing ERET, it will just detag
      * the result. However, this helper is only used for MIPS/RISC-V.
@@ -16,7 +23,9 @@
      * On exception return we unseal sentry capabilities (if the address
      * matches).
         if (new_cursor == cap_get_cursor(pcc)) {
+            return;
                          "addr: " PRINT_CAP_FMTSTR "\r",
+        if (new_cursor == cap_get_cursor(pcc)) {
     uint32_t perms = CAP_PERM_STORE;
     if (get_capreg_tag(env, cs))
         perms |= CAP_PERM_STORE_CAP;
