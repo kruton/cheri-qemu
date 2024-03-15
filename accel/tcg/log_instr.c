0c5c144a70042a7c2c635bbfb56864d3cef2a549
Martin Kaiser <martin.kaiser@codasip.com>
2024-03-15 17:52:26 +0100

    log_instr: add fallthrough maker to fix compiler warning
    Add a fallthrough marker in one place to fix a compiler warning.
    
    ../accel/tcg/log_instr.c: In function ‘qemu_log_gen_printf’:
    ../accel/tcg/log_instr.c:1327:27: warning: this statement may fall through [-Wimplicit-fallthrough=]
                     is_signed = true;
    
    Co-authored-by: Paul Buxton <paul.buxton@codasip.com>

--- accel/tcg/log_instr.c
+++ accel/tcg/log_instr.c
@@ -19,4 +19,5 @@
         qemu_log("%s", iinfo->txt_buffer->str);
     log_assert(iinfo != NULL && "Invalid log buffer");
         bool is_short = false, is_long = false, is_long_long = false, is_signed = false;
+                QEMU_FALLTHROUGH;
     qemu_log_instr_commit(env);
