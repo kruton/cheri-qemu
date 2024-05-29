eed3586ab6266b39a04dacb92c62d844ef8959e7
Paul Buxton <paul.buxton@codasip.com>
2024-05-29 15:28:03 +0100

    CHERI-RISC-V: Add enums for 0.9.3 CHERI exception types/causes.
    RiscV Cheri Spec simplified many of the exception causes to a just 4.
    An exception cause type was added to provide additional granularity.
    
    Co-authored-by: Alexander Richardson <alexrichardson@google.com>

--- target/riscv/cheri-archspecific-early.h
+++ target/riscv/cheri-archspecific-early.h
@@ -1,5 +1,23 @@
  * This controls how many tags are fetched with cgettags/csettags
 #define CAP_TAG_GET_MANY_SHFT 3
+#endif
+#ifdef TARGET_CHERI_RISCV_STD_093
+/* Cheri standard 0.9.3 Exception Types */
+typedef enum {
+    CapEx093_Type_InstrAccess = 0x0,
+    CapEx093_Type_Data = 0x1,
+    CapEx093_Type_Branch = 0x2,
+    CapEx093_Type_Last = CapEx093_Type_Branch,
+    CapEx093_Type_None = 0xff,
+} Cheri093CapExcType;
+    CapEx093_TagViolation = 0x0,
+    CapEx093_SealViolation = 0x1,
+    CapEx093_PermissionViolation = 0x2,
+    CapEx093_InvalidAddressViolation = 0x3,
+    CapEx093_BoundsViolation = 0x4,
+    CapEx093_Last = CapEx093_BoundsViolation,
+    CapEx093_None = 0xff,
+} Cheri093CapExcCause;
 #define CHERI_CONTROLFLOW_CHECK_AT_TARGET 0
 /* TODO: switch tag clearing to true once CheriBSD is ready for it. */
 #define CHERI_TAG_CLEAR_ON_INVALID 0
