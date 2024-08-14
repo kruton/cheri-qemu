92efb2dcd891ac7e88aa2e84ab60405643c01d09
Paul Buxton <paul.buxton@codasip.com>
2024-08-14 14:58:48 +0100

    log_instr: Ensure logging status is propagated to tcg
    If a code block has previously been translated prior to a logging level
    change and it is subsequently executed then the execution can occur with
    the wrong logging level resulting in confusing log output.
    When we change logging status this needs to be reflected in the cflags
    used for looking up/generating a tcg block.
    
    We can't use tb_flush here, since at qemu startup, tb_flush might be
    called before tcg is initialized and a thread is created for the vcpu.
    The sequence is:
    ```
    qemu_log_instr_init calls
       do_cpu_loglevel_switch
          tb_flush
             async_safe_run_on_cpu
                queue_work_on_cpu (cpu=<optimized out>, wi=<optimized out>) at ../cpus-common.c:131
                   qemu_cpu_kick (cpu=0x55555667e020) at ../softmmu/cpus.c:451
                      qemu_cond_broadcast (cond=0x0) at ../util/qemu-thread-posix.c:162
    ```
    This happens only if -d instr is set on the qemu commandline. Otherwise,
    the first call to do_cpu_loglevel_switch will not switch the log level.
    
    This sequence causes a crash as cpu->halt_cond is NULL in
    qemu_cond_broadcast. cpu->halt_cond is initialised by
    rr_start_vcpu_thread, which also initialises cpu->thread.
    
    This partially reverts commit 9aa3707.
    Co-authored-by: Martin Kaiser <martin.kaiser@codasip.com>
    Co-authored-by: Paul Buxton <paul.buxton@codasip.com>

--- accel/tcg/log_instr.c
+++ accel/tcg/log_instr.c
@@ -1,4 +1,6 @@
  *
+ */
+/*
  * A central desing goal is to reliably log multiple register updates and memory
  * accesses performed by an instruction. We also want to allow to log arbitary
  * events via special no-op instructions. Extra text debug output can also be
@@ -18,7 +20,18 @@
             qemu_log("  %s <- " TARGET_FMT_lx " (setting integer value)\n",
         qemu_log("    Write %s = " TARGET_FMT_lx "\n", rinfo->name,
         qemu_log("%s", iinfo->txt_buffer->str);
+    } else {
     log_assert(iinfo != NULL && "Invalid log buffer");
+     * This function is called when tcg generates code for a dummy slti
+     * instruction that changes the log level (or when qemu is started).
+     * The generated code terminates the current TB.
+     * We have to propagate the updated logging status to the next TB.
+        cpu->cflags_next_tb = curr_cflags(cpu) | CF_LOG_INSTR;
+        cpu->cflags_next_tb = curr_cflags(cpu) & ~CF_LOG_INSTR;
+     * It seems that cpu->cflags_next_tb affect only the next block, not the
+     * ones after this. Update cpu->tcg_cflags to set the updated flags for
+     * all following blocks.
+    cpu->tcg_cflags = cpu->cflags_next_tb;
 #pragma GCC diagnostic pop
         bool is_short = false, is_long = false, is_long_long = false, is_signed = false;
                 QEMU_FALLTHROUGH;
