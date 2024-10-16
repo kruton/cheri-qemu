6b9201e189aee221e68abf119349cda5221156d4
Martin Kaiser <martin.kaiser@codasip.com>
2024-10-16 16:21:24 +0200

    cmu: define properties for cmu configuration
    Someone who instantiates a cmu object will have to configure its instance.
    The config settings like ram size, location are not constant and not known
    at compile time. It's common practice in qemu to use properties for such
    settings. This patch defines the required properties for cmu configuration.

---
+++ hw/riscv/cmu.c
@@ -0,0 +1,13 @@
+#include "hw/qdev-properties.h"
+{
+    }
+};
+static const Property cmu_properties[] = {
+    DEFINE_PROP_UINT64("ram-base", CMUDeviceState, base, 0),
+    DEFINE_PROP_UINT64("ram-size", CMUDeviceState, size, 0),
+    DEFINE_PROP_LINK("managed-ram", CMUDeviceState, managed,
+            TYPE_MEMORY_REGION, MemoryRegion *),
+static void cmu_class_init(ObjectClass *oc, const void *data)
+    DeviceClass *dc = DEVICE_CLASS(oc);
+    device_class_set_props(dc, cmu_properties);
+    .class_init = cmu_class_init,
