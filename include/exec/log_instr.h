2c1eacf44f411da49cadcaac804dfb6a953cacae
Martin Kaiser <martin.kaiser@codasip.com>
2024-10-29 14:37:06 +0100

    tcg: log: remove two unused functions
    qemu_log_instr_ld_int and qemu_log_instr_st_int are not used. Remove the
    functions and their prototypes.

--- include/exec/log_instr.h
+++ include/exec/log_instr.h
@@ -1,6 +1,10 @@
+ */
+/*
 /* Should only be called in a place it cannot be skipped by a branch! */
 void qemu_log_gen_printf_flush(struct DisasContextBase *base, bool flush_early,
                                bool force_flush);
+void qemu_log_instr_ld_cap(CPUArchState *env, target_ulong addr,
                            const cap_register_t *value);
+void qemu_log_instr_st_cap(CPUArchState *env, target_ulong addr,
                         target_ulong arg1, target_ulong arg2,
                         target_ulong arg3);
