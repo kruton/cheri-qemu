569139e23ad61ec0b82176ccb37b5767881a8185
Martin Kaiser <martin.kaiser@codasip.com>
2024-12-10 00:25:25 -0800

    RVY: fix handling of C permission in amoswap.cap
    If amoswap.cap is authorized by a capability that does not grant the C
    permissions, the cpu must not throw an exception. Instead, it must clear
    the tag of the capability that is written to memory.
    This is a behaviour change compared to ISAv9, so is guarded by an ifdef.

--- target/riscv/op_helper_cheri.c
+++ target/riscv/op_helper_cheri.c
@@ -388,12 +388,14 @@ void HELPER(amoswap_cap)(CPUArchState *env, uint32_t dest_reg,
         raise_cheri_exception(env, CapEx_PermitLoadViolation, addr_reg);
     } else if (!cap_has_perms(cbp, CAP_PERM_STORE)) {
         raise_cheri_exception(env, CapEx_PermitStoreViolation, addr_reg);
+#ifndef TARGET_CHERI_RISCV_STD /* RISC-V Standard CHERI tag clears instead. */
     } else if (!cap_has_perms(cbp, CAP_PERM_STORE_CAP)) {
         raise_cheri_exception(env, CapEx_PermitStoreCapViolation, addr_reg);
     } else if (!cap_has_perms(cbp, CAP_PERM_STORE_LOCAL) &&
                get_capreg_tag(env, val_reg) &&
                !(get_capreg_hwperms(env, val_reg) & CAP_PERM_GLOBAL)) {
         raise_cheri_exception(env, CapEx_PermitStoreLocalCapViolation, val_reg);
+#endif
     }
 
     uint64_t addr = (uint64_t)(cap_get_cursor(cbp) + (target_long)offset);
