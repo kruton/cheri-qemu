856bc75c79f6def91608968ee50365ad2fc61a73
Martin Kaiser <martin.kaiser@codasip.com>
2025-01-22 17:45:29 +0100

    riscv: cbo: move some checks out of do_cbo_zero
    Move some checks out of do_cbo_zero back into helper_cbo_zero.
    
    We'll have two different cbo zero helpers for cheri and non-cheri, both
    of which will call do_cbo_zero to do the actual zeroing.
    
    The cheri helper must do things in this order
    
    1.) check if the CSRs permit the cbo.zero
    2.) align the address to the cache block size
    3.) do cheri checks on the authorizing capability and the aligned address
    4.) call do_cbo_zero
    
    1.) and 2.) have to be done in the "upper" cheri/non-cheri helpers.
    This patch moves them out of do_cbo_zero back into the non-cheri
    helper. (The cheri helper will be added shortly.)

--- target/riscv/op_helper.c
+++ target/riscv/op_helper.c
@@ -191,10 +191,9 @@ static void do_cbo_zero(CPURISCVState *env, target_ulong address, uintptr_t ra)
     int mmu_idx = riscv_env_mmu_index(env, false);
     void *mem;
 
-    check_zicbo_envcfg(env, MENVCFG_CBZE, ra);
 
-    /* Mask off low-bits to align-down to the cache-block. */
-    address &= ~(cbozlen - 1);
+    /* Caller must pass an address that is aligned-down to the cache-block. */
+    g_assert(QEMU_IS_ALIGNED(address, cbozlen));
 
     /*
      * cbo.zero requires MMU_DATA_STORE access. Do a probe_write()
@@ -224,6 +223,12 @@ static void do_cbo_zero(CPURISCVState *env, target_ulong address, uintptr_t ra)
 
 void helper_cbo_zero(CPURISCVState *env, target_ulong address)
 {
+    RISCVCPU *cpu = env_archcpu(env);
+    uint16_t cbozlen = cpu->cfg.cboz_blocksize;
+    uintptr_t ra = GETPC();
+    check_zicbo_envcfg(env, MENVCFG_CBZE, ra);
+    /* Mask off low-bits to align-down to the cache-block. */
+    address &= ~(cbozlen - 1);
     do_cbo_zero(env, address, ra);
 }
 /*
