ce1724f4287fc1b4271d9abb886c6a74bd5a2159
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2025-02-27 18:52:10 -0800

    Fix -Wredundant-decl warning for cpu_restore_state()
    Move the files that need it to cheri-helper-utils.h instead as this
    already includes exec/exec-all.h.
    While touching these lines also remove the unnecessary
    cheri_get_current_pcc_fetch_from_tcg() function which is only called
    in a situation where the pc has to be updated already.
    
    Co-authored-by: Martin Kaiser <martin.kaiser@codasip.com>

--- target/mips/cheri-archspecific.h
+++ target/mips/cheri-archspecific.h
@@ -1,4 +1,11 @@
     if (reg == CHERI_EXC_REGNUM_DDC) {
         reg = CHERI_TRUE_EXC_REGNUM_DDC;
     }
+    if (qemu_log_instr_or_mask_enabled(env, CPU_LOG_INT)) {
+        cpu_restore_state(env_cpu(env), hostpc);
+        qemu_log_instr_or_mask_msg(
+            env, CPU_LOG_INT,
+            " -> host PC: 0x%jx\n",
+            cause, cheri_cause_str(cause), reg,
+            PRINT_CAP_ARGS(cheri_get_current_pcc(env)), (uintmax_t)hostpc);
                                            cap_register_t *target,
