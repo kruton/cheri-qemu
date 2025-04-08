5f41438f6621d1a74e1d2bf6addde4fa5bbfba33
Alex Richardson <alexrichardson@google.com>
2025-04-08 14:49:11 -0700

    Move the cchecktype helper to MIPS
    This was never added to RISC-V and should not be in the common code.

--- target/mips/op_helper_cheri.c
+++ target/mips/op_helper_cheri.c
@@ -395,16 +395,24 @@ void cheri_dump_state(CPUState *cs, FILE *f, fprintf_function cpu_fprintf, int f
     cheri_dump_creg(&env->active_tc.CHWR.KDC,        "HWREG 30 (KDC)", "", f, cpu_fprintf);
     cheri_dump_creg(&env->active_tc.CHWR.EPCC,       "HWREG 31 (EPCC)", "", f, cpu_fprintf);
     cpu_fprintf(f, "\n");
+}
 void CHERI_HELPER_IMPL(mtc2_dumpcstate(CPUArchState *env, target_ulong arg1))
+{
 void CHERI_HELPER_IMPL(cchecktype(CPUArchState *env, uint32_t cs, uint32_t cb))
     GET_HOST_RETPC();
     const cap_register_t *csp = get_readonly_capreg(env, cs);
+    const cap_register_t *cbp = get_readonly_capreg(env, cb);
+    /*
      * CCheckType: Raise exception if otypes don't match
+     */
     if (!csp->cr_tag) {
         raise_cheri_exception(env, CapEx_TagViolation, cs);
     } else if (!cbp->cr_tag) {
         raise_cheri_exception(env, CapEx_TagViolation, cb);
     } else if (cap_is_unsealed(csp)) {
         raise_cheri_exception(env, CapEx_SealViolation, cs);
+    } else if (cap_is_unsealed(cbp)) {
         raise_cheri_exception(env, CapEx_SealViolation, cb);
+    } else if (cap_get_otype_unsigned(csp) != cap_get_otype_unsigned(cbp) ||
                !cap_is_sealed_with_type(csp)) {
+        raise_cheri_exception(env, CapEx_TypeViolation, cs);
