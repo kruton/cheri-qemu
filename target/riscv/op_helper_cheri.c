ffba11236521d5f0d116b3380046bbed57ec585a
Alex Richardson <alexrichardson@google.com>
2025-04-08 17:54:25 -0700

    riscv: cheri: Fix names of the hypervisor SCRs
    They no longer use a 'bs' prefix but instead use 'vs'

--- target/riscv/op_helper_cheri.c
+++ target/riscv/op_helper_cheri.c
@@ -73,13 +73,10 @@ struct SCRInfo {
     [CheriSCR_DDC] = {.r = true, .w = true, .access = U_Always, .name = "DDC"},
 
 
-    [CheriSCR_STCC] = {.r = true, .w = true, .access = S_ASR, .name = "STCC"},
-    [CheriSCR_STDC] = {.r = true, .w = true, .access = S_ASR, .name = "STDC"},
     [CheriSCR_SScratchC] = {.r = true,
                             .w = true,
                             .access = S_ASR,
                             .name = "SScratchC"},
-    [CheriSCR_SEPCC] = {.r = true, .w = true, .access = S_ASR, .name = "SEPCC"},
 
     [CheriSCR_MTCC] = {.r = true, .w = true, .access = M_ASR, .name = "MTCC"},
     [CheriSCR_MTDC] = {.r = true, .w = true, .access = M_ASR, .name = "MTDC"},
@@ -89,7 +86,6 @@ struct SCRInfo {
                             .name = "MScratchC"},
     [CheriSCR_MEPCC] = {.r = true, .w = true, .access = M_ASR, .name = "MEPCC"},
 
-    [CheriSCR_BSScratchC] = {.r = true, .w = true, .access = H_ASR,
 };
 
 {
