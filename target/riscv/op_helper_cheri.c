b2bb0253225575ac851934aee156f594c2a88038
Alex Richardson <alexrichardson@google.com>
2025-04-17 11:02:59 -0700

    CHERI-RISC-V: simplify check_csr_cap_permissions()
    Most of the checks are shared with the newly factored out
    riscv_csr_accessible(), so we can just call that for most of them.

--- target/riscv/op_helper_cheri.c
+++ target/riscv/op_helper_cheri.c
@@ -114,39 +114,12 @@ static RISCVException check_csr_cap_permissions(CPURISCVState *env, int csrno,
                                                 bool write_access,
                                                 riscv_csr_cap_ops *csr_cap_info)
 {
-    RISCVCPU *cpu = env_archcpu(env);
 
-    /* ensure the CSR extension is enabled. */
-    if (!cpu->cfg.ext_icsr) {
-        return RISCV_EXCP_ILLEGAL_INST;
-    }
-
-#if !defined(CONFIG_USER_ONLY)
-    int effective_priv = env->priv;
-    /* csr[11:10] if the csr can be read/written, 0b11 means read-only */
-    bool read_only = (get_field(csrno, 0xC00) == 3);
-
-    if (riscv_has_ext(env, RVH) && env->priv == PRV_S &&
-        !riscv_cpu_virt_enabled(env)) {
-        /*
-         * We are in S mode without virtualisation, therefore we are in HS Mode.
-         * Add 1 to the effective privlege level to allow us to access the
-         * Hypervisor CSRs.
-         */
-        effective_priv++;
-    }
-
-    if ((write_access && read_only) ||
-        (!env->debugger && (effective_priv < get_field(csrno, 0x300)))) {
-        return RISCV_EXCP_ILLEGAL_INST;
-    }
-#endif
     if ((csr_cap_info->flags & CSR_OP_REQUIRE_CRE) &&
         !riscv_cpu_mode_cre(env)) {
         return RISCV_EXCP_ILLEGAL_INST;
     }
-
-    return RISCV_EXCP_NONE;
+    return riscv_csr_accessible(env, csrno, write_access);
 }
 
 /* Copy a capability to a register, or update address if we are not capmode.*/
