c193eaefb729714b4cda96d39ea32e8736ce312e
Hesham Almatary <heshamalmatary@capabilitieslimited.co.uk>
2025-06-26 10:41:41 +0000

    RVY: Use openSBI firmware instead of bbl
    BBL is not used for RVY anymore.
    
    Co-authored-by: Alexander Richardson <alexrichardson@google.com>
    Signed-off-by: Hesham Almatary <heshamalmatary@capabilitieslimited.co.uk>

--- include/hw/riscv/boot.h
+++ include/hw/riscv/boot.h
@@ -24,8 +24,18 @@
 #include "hw/loader.h"
 #include "hw/riscv/riscv_hart.h"
 
+#if defined(TARGET_CHERI_RISCV_V9)
+/* Use a purecap BBL fw_jump as the BIOS for CHERIv9 */
+#define RISCV32_BIOS_BIN    "bbl-riscv32cheri-virt-fw_jump.bin"
+#define RISCV64_BIOS_BIN    "bbl-riscv64cheri-virt-fw_jump.bin"
+#elif defined(TARGET_CHERI_RISCV_STD)
+/* Use a purecap OpenSBI fw_jump as the BIOS for RVY. TODO: Use fw_dynamic? */
+#define RISCV32_BIOS_BIN    "opensbi-riscv32cheristd-generic-fw_jump.bin"
+#define RISCV64_BIOS_BIN    "opensbi-riscv64cheristd-generic-fw_jump.bin"
+#else
 #define RISCV32_BIOS_BIN    "opensbi-riscv32-generic-fw_dynamic.bin"
 #define RISCV64_BIOS_BIN    "opensbi-riscv64-generic-fw_dynamic.bin"
+#endif
 
 typedef struct RISCVBootInfo {
     ssize_t kernel_size;
