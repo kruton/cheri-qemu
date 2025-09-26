f5d5448615c266908aca706df80df5353a914faa
Martin Kaiser <martin.kaiser@codasip.com>
2025-09-26 18:03:20 +0200

    RVY: Correctly use tag clearing in the standard draft
    This was accidentally set to the wrong value during recent refactoring.
    
    Co-authored-by: Martin Kaiser <martin.kaiser@codasip.com>

--- target/riscv/cheri-archspecific-early.h
+++ target/riscv/cheri-archspecific-early.h
@@ -21,6 +21,8 @@ typedef enum {
 #define CHERI_CONTROLFLOW_CHECK_AT_TARGET 0
 /* TODO: switch tag clearing to true once CheriBSD is ready for it. */
 #define CHERI_TAG_CLEAR_ON_INVALID 0
+#define CHERI_TAG_CLEAR_ON_INVALID(env) true
+#define CHERI_NO_RELOCATION(env)        true
 #define CINVOKE_DATA_REGNUM 31
     cheri_debug_assert(env->ddc.cr_extra == CREG_FULLY_DECOMPRESSED);
 #define CHERI_GPCAPREGS_MEMBER gpcapregs
