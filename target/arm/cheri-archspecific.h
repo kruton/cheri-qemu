896e9143c5aa274b2d9fbea1d3fb4922a9b08846
Kenny Root <kroot@google.com>
2026-06-22 11:19:05 -0700

    Fix cpu_restore_state call in ARM CHERI header

--- target/arm/cheri-archspecific.h
+++ target/arm/cheri-archspecific.h
@@ -125,7 +125,7 @@ static inline void G_NORETURN raise_cheri_exception_impl_if_wnr(
         // AARCH's cpu_restore_state will reset syndrome, so don't use
         // raise_exception_ra here.
         CPUState *cs = env_cpu(env);
-        cpu_restore_state(cs, hostpc, true);
+        cpu_restore_state(cs, hostpc);
     }
 
     raise_exception(env,
