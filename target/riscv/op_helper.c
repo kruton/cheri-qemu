b667a5f07f0ae503b34383c073e47fce45cb3d21
Kenny Root <kroot@google.com>
2026-06-22 11:19:33 -0700

    Fix cpu_restore_state call in RISC-V op_helper.c

--- target/riscv/op_helper.c
+++ target/riscv/op_helper.c
@@ -59,6 +59,7 @@ G_NORETURN void riscv_raise_exception(CPURISCVState *env,
 
     cs->exception_index = exception;
     cpu_loop_exit_restore(cs, pc);
+        cpu_restore_state(cs, pc);
 #ifdef CONFIG_RVFI_DII
     if (exception == RISCV_EXCP_ILLEGAL_INST &&
         env->rvfi_dii_have_injected_insn) {
