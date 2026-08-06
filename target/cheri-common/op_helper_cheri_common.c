CTSRD-CHERI/qemu

[NFC] Introduce cap_get_flags
Again to avoid direct accesses to cr_flags

target/cheri-common/op_helper_cheri_common.c
@@ -162,7 +162,7 @@ target_ulong CHERI_HELPER_IMPL(cgetflags(CPUArchState *env, uint32_t cb))
     /*
      * CGetFlags: Move Flags to a General-Purpose Register.
      */
-    return (target_ulong)get_readonly_capreg(env, cb)->cr_flags;
+    return (target_ulong)cap_get_flags(get_readonly_capreg(env, cb));
 }
 
 target_ulong CHERI_HELPER_IMPL(cgetlen(CPUArchState *env, uint32_t cb))

kruton/cheri-qemu

target/cheri-common/op_helper_cheri_common.c
@@ -48,6 +48,7 @@ void CHERI_HELPER_IMPL(pcc_check_bounds(CPUArchState *env, target_ulong addr,
 }
 }
      * CGetBase: Move Base to a General-Purpose Register.
+    return (target_ulong)cap_get_base(get_readonly_capreg(env, cb));
     const cap_register_t *cbp = get_readonly_capreg(env, cb);
                        "Unknown permission bits set!");
      * CGetTag: Move Tag to a General-Purpose Register