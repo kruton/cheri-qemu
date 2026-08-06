CTSRD-CHERI/qemu

Fix disassembly of CHERI-RISCV stores
sd is not the same as ld.

disas/riscv.c
@@ -1754,14 +1754,14 @@ static void decode_inst_opcode(rv_decode *dec, rv_isa isa, int flags)
             break;
         case 8:
             switch (((inst >> 12) & 0b111)) {
-            case 0: op = rv_op_sb; break;
-            case 1: op = rv_op_sh; break;
-            case 2: op = rv_op_sw; break;
+            case 0: op = (flags & RISCV_DIS_FLAG_CAPMODE) ? rv_op_csb : rv_op_sb; break;
+            case 1: op = (flags & RISCV_DIS_FLAG_CAPMODE) ? rv_op_csh : rv_op_sh; break;
+            case 2: op = (flags & RISCV_DIS_FLAG_CAPMODE) ? rv_op_csw : rv_op_sw; break;
             case 3:
                 if (isa == rv32 && flags & RISCV_DIS_FLAG_CHERI) {
                     op = (flags & RISCV_DIS_FLAG_CAPMODE) ? rv_op_csc : rv_op_sc;
                 } else {
-                    op = (flags & RISCV_DIS_FLAG_CAPMODE) ? rv_op_csd : rv_op_ld;
+                    op = (flags & RISCV_DIS_FLAG_CAPMODE) ? rv_op_csd : rv_op_sd;
                 }
                 break;
             case 4:

kruton/cheri-qemu

disas/riscv.c
@@ -2934,13 +2934,14 @@ static rv_opcode decode_cheri_inst(rv_inst inst) {
         switch ((inst >> 2) & 0b11111) {
         case 0:
             switch ((inst >> 12) & 0b111) {
-            case 0: op = rv_op_lb; break;
-            case 1: op = rv_op_lh; break;
-            case 2: op = rv_op_lw; break;
             case 3: op = rv_op_ld; break;
             case 4: op = rv_op_lbu; break;
             case 5: op = rv_op_lhu; break;
             case 6: op = rv_op_lwu; break;
+            case 0: op = (flags & RISCV_DIS_FLAG_CAPMODE) ? rv_op_clb : rv_op_lb; break;
+            case 1: op = (flags & RISCV_DIS_FLAG_CAPMODE) ? rv_op_clh : rv_op_lh; break;
+            case 2: op = (flags & RISCV_DIS_FLAG_CAPMODE) ? rv_op_clw : rv_op_lw; break;
+                    op = (flags & RISCV_DIS_FLAG_CAPMODE) ? rv_op_cld : rv_op_ld;
             case 7: op = rv_op_ldu; break;
             }
             break;