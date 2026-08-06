CTSRD-CHERI/qemu

Fix target_disas_buf()

target/riscv/translate.c
@@ -906,7 +906,7 @@ static void riscv_tr_disas_log(const DisasContextBase *dcbase, CPUState *cpu)
         uint32_t insn = env->rvfi_dii_trace.rvfi_dii_insn;
         if (logfile) {
             fprintf(logfile, "IN: %s\n", lookup_symbol(dcbase->pc_first));
-            disas(logfile, &insn, sizeof(insn));
+            target_disas_buf(stderr, cpu, &insn, sizeof(insn), dcbase->pc_first, 1);
         }
         qemu_log_unlock(logfile);
     }

kruton/cheri-qemu

target/riscv/translate.c
@@ -1437,6 +1437,7 @@ static void riscv_tr_tb_stop(DisasContextBase *dcbase, CPUState *cpu)
         assert(dcbase->num_insns == 1);
         uint32_t insn = env->rvfi_dii_injected_insn;
         fprintf(logfile, "IN: %s\n", lookup_symbol(dcbase->pc_first));
+        target_disas_buf(logfile, cpu, &insn, sizeof(insn), dcbase->pc_first, 1);
     }
 static const TranslatorOps riscv_tr_ops = {
     .init_disas_context = riscv_tr_init_disas_context,