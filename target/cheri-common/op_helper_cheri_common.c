CTSRD-CHERI/qemu

Use target_ulong instead of uint64_t
No need to extend+truncate for 32-bit builds

target/cheri-common/op_helper_cheri_common.c
@@ -1126,7 +1126,7 @@ void CHERI_HELPER_IMPL(store_cap_via_cap(CPUArchState *env, uint32_t cs,
     // in the hybrid ABI (and also for backwards compat with old binaries).
     const cap_register_t *cbp = get_load_store_base_cap(env, cb);
 
-    const uint64_t addr =
+    const target_ulong addr =
         cap_check_common_reg(perms_for_store(env, cs), env, cb, offset,
                              CHERI_CAP_SIZE, _host_return_address, cbp,
                              CHERI_CAP_SIZE, raise_unaligned_store_exception);

kruton/cheri-qemu

target/cheri-common/op_helper_cheri_common.c
@@ -114,6 +114,7 @@ void CHERI_HELPER_IMPL(candaddr(CPUArchState *env, uint32_t cd, uint32_t cb,
     target_ulong offset, uint32_t size))
     return cap_check_common(CAP_PERM_LOAD | CAP_PERM_STORE, env, cb, offset,
 target_ulong CHERI_HELPER_IMPL(cap_check_addr(CPUArchState *env,
+    const target_ulong checked_addr =
         if (cap_is_unsealed(&tmp)) {
         CAP_cc(decompress_raw_ext)(*pesbt, *cursor, tag, lvbits, &ncd);
         tag = cheri_tag_prot_clear_or_trap(env, vaddr, cb, source, prot, retpc,
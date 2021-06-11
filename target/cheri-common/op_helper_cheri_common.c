79abc516bcf64fcdebd097d247aeeba07395c33f
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2021-06-11 16:27:45 +0100

    Use target_ulong instead of uint64_t
    No need to extend+truncate for 32-bit builds

--- target/cheri-common/op_helper_cheri_common.c
+++ target/cheri-common/op_helper_cheri_common.c
@@ -114,6 +114,7 @@ void CHERI_HELPER_IMPL(candaddr(CPUArchState *env, uint32_t cd, uint32_t cb,
     target_ulong offset, uint32_t size))
     return cap_check_common(CAP_PERM_LOAD | CAP_PERM_STORE, env, cb, offset,
 target_ulong CHERI_HELPER_IMPL(cap_check_addr(CPUArchState *env,
+    const target_ulong checked_addr =
         if (cap_is_unsealed(&tmp)) {
         CAP_cc(decompress_raw_ext)(*pesbt, *cursor, tag, lvbits, &ncd);
         tag = cheri_tag_prot_clear_or_trap(env, vaddr, cb, source, prot, retpc,
