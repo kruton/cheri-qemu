arichardson/qemu

mips: Fix stderr being locked when using dumpstate helpers
If we don't specify a logfile path qemu_log_lock() will return stderr
and we never unlock the file.
Fix the condition to match the one above.

target/mips/op_helper_log_instr.c
@@ -318,14 +318,9 @@ static void simple_dump_state(CPUMIPSState *env, FILE *f,
 
 void helper_mtc0_dumpstate(CPUMIPSState *env, target_ulong arg1)
 {
-    FILE* logfile = qemu_log_enabled() ? qemu_log_lock() : stderr;
-#if 0
-    cpu_dump_state(env_cpu(env), logfile, fprintf, CPU_DUMP_CODE);
-#else
-    simple_dump_state(env, logfile, fprintf);
-#endif
-    if (logfile != stderr)
-        qemu_log_unlock(logfile);
+    FILE *logfile = qemu_log_lock();
+    simple_dump_state(env, logfile ? logfile : stderr, fprintf);
+    qemu_log_unlock(logfile);
 }
 
 void do_hexdump(GString *strbuf, uint8_t* buffer, target_ulong length,

kruton/cheri-qemu

target/mips/op_helper_log_instr.c
@@ -101,5 +101,8 @@ static void simple_dump_state(CPUMIPSState *env, FILE *f,
     cpu_fprintf(f, "DEBUG MIPS REG 29 0x" TARGET_FMT_lx "\n", env->active_tc.gpr[29]);
     cpu_fprintf(f, "DEBUG MIPS REG 30 0x" TARGET_FMT_lx "\n", env->active_tc.gpr[30]);
     cpu_fprintf(f, "DEBUG MIPS REG 31 0x" TARGET_FMT_lx "\n", env->active_tc.gpr[31]);
+    FILE *logfile = qemu_log_trylock();
+    simple_dump_state(env, logfile ? logfile : stderr, fprintf);
+    qemu_log_unlock(logfile);
     target_ulong addr;
         for (addr = line_start; addr < vaddr; addr++) {