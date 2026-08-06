a-richardson/qemu

[CHERI-RISC-V] CUnseal: use tag-clearing semantics

target/cheri-common/op_helper_cheri_common.c
@@ -771,7 +771,8 @@ void CHERI_HELPER_IMPL(cseal(CPUArchState *env, uint32_t cd, uint32_t cs,
 void CHERI_HELPER_IMPL(cunseal(CPUArchState *env, uint32_t cd, uint32_t cs,
                                uint32_t ct))
 {
-    GET_HOST_RETPC();
+    GET_HOST_RETPC_IF_TRAPPING_CHERI_ARCH();
+    DEFINE_RESULT_VALID;
     const cap_register_t *csp = get_readonly_capreg(env, cs);
     const cap_register_t *ctp = get_readonly_capreg(env, ct);
     const target_ulong ct_cursor = cap_get_cursor(ctp);
@@ -779,40 +780,46 @@ void CHERI_HELPER_IMPL(cunseal(CPUArchState *env, uint32_t cd, uint32_t cs,
      * CUnseal: Unseal a sealed capability
      */
     if (!csp->cr_tag) {
-        raise_cheri_exception(env, CapEx_TagViolation, cs);
+        raise_cheri_exception_or_invalidate(env, CapEx_TagViolation, cs);
     } else if (!ctp->cr_tag) {
-        raise_cheri_exception(env, CapEx_TagViolation, ct);
+        raise_cheri_exception_or_invalidate(env, CapEx_TagViolation, ct);
     } else if (cap_is_unsealed(csp)) {
-        raise_cheri_exception(env, CapEx_SealViolation, cs);
+        raise_cheri_exception_or_invalidate(env, CapEx_SealViolation, cs);
     } else if (!cap_is_unsealed(ctp)) {
-        raise_cheri_exception(env, CapEx_SealViolation, ct);
+        raise_cheri_exception_or_invalidate(env, CapEx_SealViolation, ct);
     } else if (!cap_is_sealed_with_type(csp)) {
-        raise_cheri_exception(env, CapEx_TypeViolation,
-                              cs); /* Reserved otypes */
+        /* Reserved otypes are not allowed. */
+        raise_cheri_exception_or_invalidate(env, CapEx_TypeViolation, cs);
     } else if (ct_cursor != cap_get_otype_unsigned(csp)) {
-        raise_cheri_exception(env, CapEx_TypeViolation, ct);
+        raise_cheri_exception_or_invalidate(env, CapEx_TypeViolation, ct);
     } else if (!cap_has_perms(ctp, CAP_PERM_UNSEAL)) {
-        raise_cheri_exception(env, CapEx_PermitUnsealViolation, ct);
+        raise_cheri_exception_or_invalidate(env, CapEx_PermitUnsealViolation,
+                                            ct);
     } else if (!cap_cursor_in_bounds(ctp)) {
-        // Must be within bounds and not one past end (i.e. not equal to top).
-        raise_cheri_exception(env, CapEx_LengthViolation, ct);
+        /* Must be in bounds and not one past end (i.e. not equal to top). */
+        raise_cheri_exception_or_invalidate(env, CapEx_LengthViolation, ct);
     } else if (ct_cursor > CAP_MAX_REPRESENTABLE_OTYPE ||
                cap_otype_is_reserved(ct_cursor)) {
-        // This should never happen due to the ct_cursor != cs_otype check.
-        raise_cheri_exception(env, CapEx_LengthViolation, ct);
-    } else {
-        cap_register_t result = *csp;
-        target_ulong new_perms = cap_get_perms(&result);
-        if (cap_has_perms(csp, CAP_PERM_GLOBAL) &&
-            cap_has_perms(ctp, CAP_PERM_GLOBAL)) {
-            new_perms |= CAP_PERM_GLOBAL;
-        } else {
-            new_perms &= ~CAP_PERM_GLOBAL;
-        }
-        CAP_cc(update_perms)(&result, new_perms);
-        cap_set_unsealed(&result);
-        update_capreg(env, cd, &result);
+        /* This should never happen due to the ct_cursor != cs_otype check. */
+        raise_cheri_exception_or_invalidate(env, CapEx_LengthViolation, ct);
     }
+
+    cap_register_t result = *csp;
+    target_ulong new_perms = cap_get_perms(&result);
+    if (cap_has_perms(csp, CAP_PERM_GLOBAL) &&
+        cap_has_perms(ctp, CAP_PERM_GLOBAL)) {
+        new_perms |= CAP_PERM_GLOBAL;
+    } else {
+        new_perms &= ~CAP_PERM_GLOBAL;
+    }
+    CAP_cc(update_perms)(&result, new_perms);
+    if (RESULT_VALID) {
+        cap_set_unsealed(&result);
+    } else {
+        result.cr_tag = 0; /* Detag on invalid input/argument. */
+        CAP_cc(update_otype)(&result, CAP_OTYPE_UNSEALED);
+    }
+    update_capreg(env, cd, &result);
 }
 
 /// Three operands (capability capability int)

kruton/cheri-qemu

target/cheri-common/op_helper_cheri_common.c
@@ -18,6 +18,7 @@
 {
 #endif
     if (unlikely(cptr->cr_tag && is_cap_sealed(cptr))) {
+        raise_cheri_exception_or_invalidate_impl(env, CapEx_SealViolation,
     }
 #ifndef TARGET_MORELLO
     if (likely(addr_in_cap_bounds(cptr, new_addr))) {
@@ -26,6 +27,8 @@
     /* Result is out-of-bounds, check if it's representable. */
 #endif
         }
+        cap_register_t result = *cptr;
+    } else {
         /* (Possibly) out-of-bounds but still representable. */
         check_out_of_bounds_stat(env, oob_info,
                                  get_readonly_capreg(env, regnum_dst),
@@ -55,9 +58,11 @@ void CHERI_HELPER_IMPL(pcc_check_bounds(CPUArchState *env, target_ulong addr,
 {
 }
 }
+}
      * CGetBase: Move Base to a General-Purpose Register.
     return (target_ulong)cap_get_base(get_readonly_capreg(env, cb));
     const cap_register_t *cbp = get_readonly_capreg(env, cb);
+    target_ulong perms = cap_get_all_perms(cbp);
                        "Unknown permission bits set!");
 #endif
      * CGetTag: Move Tag to a General-Purpose Register
@@ -82,12 +87,20 @@ void CHERI_HELPER_IMPL(cjalr(CPUArchState *env, uint32_t cd,
     const target_ulong cursor = cap_get_cursor(cbp);
     GET_HOST_RETPC();
         raise_cheri_exception_branch(env, CapEx_SealViolation, data_regnum);
+        raise_cheri_exception_branch(env, CapEx_TypeViolation, code_regnum);
     } else if (!cap_has_perms(code_cap, CAP_PERM_CINVOKE)) {
     } else if (!cap_has_perms(data_cap, CAP_PERM_CINVOKE)) {
     } else if (!cap_has_perms(code_cap, CAP_PERM_EXECUTE)) {
+        raise_cheri_exception_branch(env, CapEx_LengthViolation, code_regnum);
     GET_HOST_RETPC_IF_TRAPPING_CHERI_ARCH();
+        raise_cheri_exception_or_invalidate(env, CapEx_TagViolation, cs);
     } else if (!cap_is_unsealed(csp)) {
+        raise_cheri_exception_or_invalidate(env, CapEx_SealViolation, cs);
+        raise_cheri_exception(env, CapEx_PermitExecuteViolation, cs);
     cap_register_t result = *csp;
+    if (!RESULT_VALID) {
+    CAP_cc(update_otype)(&result, CAP_OTYPE_SENTRY);
+    update_capreg(env, cd, &result);
                                   target_ulong rt))
         raise_cheri_exception(env, CapEx_TagViolation, cs);
     } else if ((cap_get_all_perms(csp) & rt) != rt) {
@@ -112,6 +125,7 @@ void CHERI_HELPER_IMPL(cjalr(CPUArchState *env, uint32_t cd,
 void CHERI_HELPER_IMPL(cbuildcap(CPUArchState *env, uint32_t cd, uint32_t cb,
                                  uint32_t ct))
 {
+    GET_HOST_RETPC_IF_TRAPPING_CHERI_ARCH();
     DEFINE_RESULT_VALID;
     cap_register_t result = *ctp;
     if (cb == 0) {
@@ -131,6 +145,7 @@ void CHERI_HELPER_IMPL(cbuildcap(CPUArchState *env, uint32_t cd, uint32_t cb,
             cap_make_sealed_entry(&derived);
             /*
              */
+    DEFINE_RESULT_VALID;
             RESULT_VALID = false;
             /* For reserved otypes we return a null-derived value. */
 static void cseal_common(CPUArchState *env, uint32_t cd, uint32_t cs,
@@ -181,6 +196,7 @@ void CHERI_HELPER_IMPL(candaddr(CPUArchState *env, uint32_t cd, uint32_t cb,
     return cap_check_common(CAP_PERM_LOAD | CAP_PERM_STORE, env, cb, offset,
 target_ulong CHERI_HELPER_IMPL(cap_check_addr(CPUArchState *env,
                                               uint32_t required_perms))
+    GET_HOST_RETPC();
     const cap_register_t *ddc = cheri_get_ddc(env);
     const target_ulong checked_addr =
     if (tag && (prot & PAGE_LC_CLEAR)) {