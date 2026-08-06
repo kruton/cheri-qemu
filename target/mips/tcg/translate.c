CTSRD-CHERI/qemu

Bug Fix: Should give Coprocessor Unusable exception.
CHERI coprocessor should give Coprocessor Unusable (EXCP_CpU)
exception, not EXCP_RI, when disabled.

target-mips/translate.c
@@ -1823,7 +1823,7 @@ static inline void check_cop2x(DisasContext *ctx)
 {
 
     if (unlikely(!(ctx->hflags & MIPS_HFLAG_COP2X))) {
-        generate_exception(ctx, EXCP_RI);
+        generate_exception(ctx, EXCP_CpU);
     }
 }
 

kruton/cheri-qemu

target/mips/tcg/translate.c
@@ -2334,6 +2334,7 @@ static void gen_st_cond(DisasContext *ctx, int rt, int base, int offset,
     /* compare the address against that of the preceding LL */
     gen_base_offset_addr(ctx, addr, base, offset);
     tcg_gen_brcond_tl(TCG_COND_EQ, addr, cpu_lladdr, l1);
+    generate_exception(ctx, EXCP_AdES);
     gen_store_gpr(tcg_constant_tl(0), rt);
     tcg_gen_br(done);
 