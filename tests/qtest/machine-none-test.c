CTSRD-CHERI/qemu

Add default CPU types for the CHERI binaries
This is needed to get unit tests passing.

tests/qtest/machine-none-test.c
@@ -39,7 +39,9 @@ static struct arch2cpu cpus_map[] = {
     { "mips", "4Kc" },
     { "mipsel", "I7200" },
     { "mips64", "20Kc" },
+    { "mips64cheri128", "BERI" },
     { "mips64el", "I6500" },
+    { "morello", "morello" },
     { "moxie", "MoxieLite" },
     { "nios2", "FIXME" },
     { "or1k", "or1200" },
@@ -56,7 +58,9 @@ static struct arch2cpu cpus_map[] = {
     { "xtensaeb", "fsf" },
     { "hppa", "hppa" },
     { "riscv64", "rv64" },
+    { "riscv64cheri", "rv64" },
     { "riscv32", "rv32" },
+    { "riscv32cheri", "rv32" },
     { "rx", "rx62n" },
 };
 

kruton/cheri-qemu

tests/qtest/machine-none-test.c
@@ -36,7 +36,9 @@ static struct arch2cpu cpus_map[] = {
     { "mips", "4Kc" },
     { "mipsel", "I7200" },
     { "mips64", "20Kc" },
+    { "mips64cheri128", "BERI" },
     { "mips64el", "I6500" },
+    { "morello", "morello" },
     { "or1k", "or1200" },
     { "ppc", "604" },
     { "ppc64", "power8e_v2.1" },
@@ -50,7 +52,9 @@ static struct arch2cpu cpus_map[] = {
     { "xtensaeb", "fsf" },
     { "hppa", "hppa" },
     { "riscv64", "rv64" },
+    { "riscv64xcheri", "rv64" },
     { "riscv32", "rv32" },
+    { "riscv32xcheri", "rv32" },
     { "rx", "rx62n" },
     { "loongarch64", "la464"},
 };