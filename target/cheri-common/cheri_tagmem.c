CTSRD-CHERI/qemu

Fix CONFIG_DEBUG_TCG CHERI binaries
9543fb1257f2e45e963b0589400fa6e9374a8812 chnaged one write->isWrite, but
missed the one in the CONFIG_DEBUG_TCG block.
This fixes booting CheriBSD on a debug qemu-system-riscv64cheri

target/cheri-common/cheri_tagmem.c
@@ -296,7 +296,7 @@ static inline void *get_tagmem_from_iotlb_entry(CPUArchState *env,
      */
 #ifdef CONFIG_DEBUG_TCG
     CPUTLBEntry *entry = tlb_entry(env, mmu_idx, vaddr);
-    g_assert(tlb_hit(write ? tlb_addr_write(entry) : entry->addr_read, vaddr));
+    g_assert(tlb_hit(isWrite ? tlb_addr_write(entry) : entry->addr_read, vaddr));
 #endif
     CPUIOTLBEntry *iotlbentry =
         &env_tlb(env)->d[mmu_idx].iotlb[tlb_index(env, mmu_idx, vaddr)];

kruton/cheri-qemu

target/cheri-common/cheri_tagmem.c
@@ -87,6 +87,7 @@ static inline QEMU_ALWAYS_INLINE void tagblock_clear_tag(CheriTagBlock *block,
      * matching tlb entry + iotlb entry.
 #ifdef CONFIG_DEBUG_TCG
     CPUTLBEntry *entry = cheri_tlb_entry(env_cpu(env), mmu_idx, vaddr);
+    g_assert(tlb_hit(isWrite ? cheri_tlb_addr_write(entry) : entry->addr_read, vaddr));
 #if defined(CHERI_UNALIGNED)
     if (unlikely((first_addr & TARGET_PAGE_MASK) !=
         warn_report("Got unaligned %d-byte store across page "