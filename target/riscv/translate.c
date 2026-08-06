CTSRD-CHERI/qemu

[CHERI-RISCV] Use capability registers for loads/stores in capmode
Surprisingly easy (assuming this actually works)

target/riscv/translate.c
@@ -776,15 +776,16 @@ static bool gen_shift(DisasContext *ctx, arg_r *a,
 }
 
 /* Include insn module translation function */
+#ifdef TARGET_CHERI
+/* Must be included first since the helpers are used by trans_rvi.inc.c */
+#include "insn_trans/trans_cheri.inc.c"
+#endif
 #include "insn_trans/trans_rvi.inc.c"
 #include "insn_trans/trans_rvm.inc.c"
 #include "insn_trans/trans_rva.inc.c"
 #include "insn_trans/trans_rvf.inc.c"
 #include "insn_trans/trans_rvd.inc.c"
 #include "insn_trans/trans_privileged.inc.c"
-#ifdef TARGET_CHERI
-#include "insn_trans/trans_cheri.inc.c"
-#endif
 
 /* Include the auto-generated decoder for 16 bit insn */
 #include "decode_insn16.inc.c"

kruton/cheri-qemu

target/riscv/translate.c
@@ -1202,6 +1202,10 @@ static uint32_t opcode_at(DisasContextBase *dcbase, target_ulong pc)
 #define SS_MMU_INDEX(ctx) (ctx->mem_idx | MMU_IDX_SS_WRITE)
 
 /* Include insn module translation function */
+#ifdef TARGET_CHERI
+/* Must be included first since the helpers are used by trans_rvi.c.inc */
+#include "insn_trans/trans_cheri.c.inc"
+#endif
 #include "insn_trans/trans_rvi.c.inc"
 #include "insn_trans/trans_rvm.c.inc"
 #include "insn_trans/trans_rva.c.inc"