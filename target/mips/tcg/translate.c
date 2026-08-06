CTSRD-CHERI/qemu

Fix for the case when a branch from the 3D extensions is in the branch delay slot of JR. This patch was originally from Brian Campbell.

target-mips/translate.c
@@ -10590,7 +10590,7 @@ static void gen_compute_branch1(DisasContext *ctx, uint32_t op,
     const char *opn = "cp1 cond branch";
     TCGv_i32 t0 = tcg_temp_new_i32();
 
-    if ((ctx->insn_flags & ISA_MIPS32R6) && (ctx->hflags & MIPS_HFLAG_BMASK)) {
+    if (ctx->hflags & MIPS_HFLAG_BMASK) {
         MIPS_DEBUG("CTI in delay / forbidden slot");
         generate_exception(ctx, EXCP_RI);
         goto out;

kruton/cheri-qemu

target/mips/tcg/translate.c
@@ -15383,6 +15383,7 @@ static void mips_tr_init_disas_context(DisasContextBase *dcbase, CPUState *cs)
               ctx->hflags);
 }
 
+    if (ctx->hflags & MIPS_HFLAG_UM) {
 static void mips_tr_tb_start(DisasContextBase *dcbase, CPUState *cs)
 {
 }