CTSRD-CHERI/qemu

[CHERI-RISCV] GDBstub: Fix order of $pcc+ddc

target/riscv/gdbstub.c
@@ -427,9 +427,9 @@ static int riscv_gdb_get_cheri_reg(CPURISCVState *env, uint8_t *mem_buf, int n)
     }
     switch (n) {
     case CHERI_GDB_NUM_GP_CAPREGS:
-        return gdb_get_capreg(mem_buf, cheri_get_ddc(env));
-    case CHERI_GDB_NUM_GP_CAPREGS + 1:
         return gdb_get_capreg(mem_buf, cheri_get_pcc(env));
+    case CHERI_GDB_NUM_GP_CAPREGS + 1:
+        return gdb_get_capreg(mem_buf, cheri_get_ddc(env));
     case CHERI_GDB_NUM_CAPREGS: {   // First integer register is the tag mask:
         uint64_t cap_valid;
         int i;

kruton/cheri-qemu

target/riscv/gdbstub.c
@@ -266,6 +266,7 @@ static int riscv_gdb_get_cheri_reg(CPUState *cs, GByteArray *buf, int n)
     }
     switch (n) {
     case CHERI_GDB_NUM_GP_CAPREGS:
+        return gdb_get_capreg(buf, cheri_get_current_pcc(env));
     case CHERI_GDB_NUM_GP_CAPREGS + 1:
         return gdb_get_capreg(buf, cheri_get_ddc(env));
     /* All CHERI registers are read-only currently.  */