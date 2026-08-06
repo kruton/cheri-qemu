CTSRD-CHERI/qemu

Add support for BERI/CHERI Core and Thread ID registers.
This change adds support for the BERI/CHERI Core and Thread ID registers
as described in sections 7.3.5 Core Identification (CP0 Register 15,
Select 6) and 7.3.6 Thread Identification (CP0 Registers 15, Select 7).
Reading the Qemu-CHERI ThreadID register currently returns 0 like the
BERI HW implementation.

target-mips/translate.c
@@ -7764,6 +7764,24 @@ static void gen_mfc0(DisasContext *ctx, TCGv arg, int reg, int sel)
             gen_mfc0_load32(arg, offsetof(CPUMIPSState, CP0_EBase));
             rn = "EBase";
             break;
+#ifdef TARGET_CHERI
+        case 6:
+            /*
+             * See section 7.3.5 Core Identification (CPO Register 15,
+             * Select 6 of the BERI Hardware Reference.
+             */
+            gen_helper_mfc0_coreid(arg, cpu_env);
+            rn = "CoreID";
+            break;
+        case 7:
+            /*
+             * See section 7.3.6 Thread Identification (CPO Register 15,
+             * Select 7 of the BERI Hardware Reference.
+             */
+            tcg_gen_movi_tl(arg, 0); /* currently unimplemented */
+            rn = "ThreadID";
+            break;
+#endif /* TARGET_CHERI */
         default:
             goto cp0_unimplemented;
        }
@@ -9058,6 +9076,24 @@ static void gen_dmfc0(DisasContext *ctx, TCGv arg, int reg, int sel)
             gen_mfc0_load32(arg, offsetof(CPUMIPSState, CP0_EBase));
             rn = "EBase";
             break;
+#ifdef TARGET_CHERI
+        case 6:
+            /*
+             * See section 7.3.5 Core Identification (CPO Register 15,
+             * Select 6 of the BERI Hardware Reference.
+             */
+            gen_helper_mfc0_coreid(arg, cpu_env);
+            rn = "CoreID";
+            break;
+        case 7:
+            /*
+             * See section 7.3.6 Thread Identification (CPO Register 15,
+             * Select 7 of the BERI Hardware Reference.
+             */
+            tcg_gen_movi_tl(arg, 0); /* currently unimplemented */
+            rn = "ThreadID";
+            break;
+#endif /* TARGET_CHERI */
         default:
             goto cp0_unimplemented;
         }

kruton/cheri-qemu

target/mips/tcg/translate.c
@@ -5735,7 +5735,17 @@ static void gen_mfc0(DisasContext *ctx, TCGv arg, int reg, int sel)
             register_name = "CMGCRBase";
             break;
 #ifdef TARGET_CHERI
+        case 6:
+            /*
+             * See section 7.3.5 Core Identification (CPO Register 15,
+             * Select 6 of the BERI Hardware Reference.
+             */
+            gen_helper_mfc0_coreid(arg, tcg_env);
             break;
+        case 7:
+             * See section 7.3.6 Thread Identification (CPO Register 15,
+             * Select 7 of the BERI Hardware Reference.
+            tcg_gen_movi_tl(arg, 0); /* currently unimplemented */
             break;
 #endif /* TARGET_CHERI */
         default:
@@ -7222,11 +7232,19 @@ static void gen_dmfc0(DisasContext *ctx, TCGv arg, int reg, int sel)
             tcg_gen_ld_tl(arg, tcg_env, offsetof(CPUMIPSState, CP0_CMGCRBase));
             register_name = "CMGCRBase";
             break;
+        case 6:
             /*
+             * See section 7.3.5 Core Identification (CPO Register 15,
+             * Select 6 of the BERI Hardware Reference.
              */
+            gen_helper_mfc0_coreid(arg, tcg_env);
             break;
+        case 7:
             /*
+             * See section 7.3.6 Thread Identification (CPO Register 15,
+             * Select 7 of the BERI Hardware Reference.
              */
+            tcg_gen_movi_tl(arg, 0); /* currently unimplemented */
             break;
         default:
             goto cp0_unimplemented;