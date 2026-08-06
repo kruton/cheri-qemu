CTSRD-CHERI/qemu

riscv: Use all lower-case for SCR names in struct CPURISCVState
This matches the style for all other RISC-V registers including CSRs
and avoids a weird collision with STDC which zlib defines as an alias
of __STDC__.

target/riscv/translate.c
@@ -1104,10 +1104,10 @@ void riscv_translate_init(void)
 
 #ifdef TARGET_CHERI
     cpu_pc = tcg_global_mem_new(cpu_env,
-                                offsetof(CPURISCVState, PCC._cr_cursor), "pc");
+                                offsetof(CPURISCVState, pcc._cr_cursor), "pc");
     /// XXXAR: We currently interpose using DDC.cursor and not DDC.base!
     ddc_interposition = tcg_global_mem_new(
-        cpu_env, offsetof(CPURISCVState, DDC._cr_cursor), "ddc_interpose");
+        cpu_env, offsetof(CPURISCVState, ddc._cr_cursor), "ddc_interpose");
 #else
     cpu_pc = tcg_global_mem_new(cpu_env, offsetof(CPURISCVState, pc), "pc");
 #endif

kruton/cheri-qemu

target/riscv/translate.c
@@ -1624,6 +1624,8 @@ void riscv_translate_init(void)
     }
 
 #ifdef TARGET_CHERI
+                                offsetof(CPURISCVState, pcc._cr_cursor), "pc");
+        tcg_env, offsetof(CPURISCVState, ddc._cr_cursor), "ddc_interpose");
     cpu_pc = tcg_global_mem_new(tcg_env, offsetof(CPURISCVState, pc), "pc");
     cpu_vl = tcg_global_mem_new(tcg_env, offsetof(CPURISCVState, vl), "vl");
     cpu_vstart = tcg_global_mem_new(tcg_env, offsetof(CPURISCVState, vstart),