CTSRD-CHERI/qemu

Add PCC READABLE TB flags
They will be used in a follow-up commit to speed up PCC permission checks.

target/cheri-common/cpu_cheri.h
@@ -136,6 +136,12 @@ static inline void cheri_cpu_get_tb_cpu_state(const cap_register_t *pcc,
     *cs_top = cap_get_top(pcc);
     *cheri_flags |=
         cheri_cap_perms_valid_for_exec(pcc) ? TB_FLAG_CHERI_PCC_VALID : 0;
+
+    if (cap_has_perms(pcc, CAP_PERM_LOAD))
+        *cheri_flags |= TB_FLAG_CHERI_PCC_READABLE;
+    if (cap_has_perms(pcc, CAP_PERM_STORE))
+        *cheri_flags |= TB_FLAG_CHERI_PCC_WRITABLE;
+
     if (*cs_base == 0 && cap_get_top_full(pcc) == CAP_MAX_TOP) {
         *cheri_flags |= TB_FLAG_CHERI_PCC_FULL_AS;
     }

kruton/cheri-qemu

target/cheri-common/cpu_cheri.h
@@ -1,4 +1,8 @@
     cheri_debug_assert(!pcc->cr_tag || cap_is_unsealed(pcc) ||
                        pcc->_cr_cursor == pc_addr);
     if (pcc->cr_tag && cap_is_unsealed(pcc)) {
+        if (cap_has_perms(pcc, CAP_PERM_LOAD))
+            *cheri_flags |= TB_FLAG_CHERI_PCC_READABLE;
         *cheri_flags |= TB_FLAG_CHERI_PCC_TOP_MAX;
+        if (cap_has_perms(ddc, CAP_PERM_STORE))
+            *cheri_flags |= TB_FLAG_CHERI_DDC_WRITABLE;
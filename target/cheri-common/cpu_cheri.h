fc27de8828a22d6b9864afc2fef7e2619cbd09b8
Lawrence Esswood <le277@cam.ac.uk>
2020-12-01 14:00:36 +0000

    Add PCC READABLE TB flags
    They will be used in a follow-up commit to speed up PCC permission checks.

--- target/cheri-common/cpu_cheri.h
+++ target/cheri-common/cpu_cheri.h
@@ -1,4 +1,8 @@
     cheri_debug_assert(!pcc->cr_tag || cap_is_unsealed(pcc) ||
                        pcc->_cr_cursor == pc_addr);
     if (pcc->cr_tag && cap_is_unsealed(pcc)) {
+        if (cap_has_perms(pcc, CAP_PERM_LOAD))
+            *cheri_flags |= TB_FLAG_CHERI_PCC_READABLE;
         *cheri_flags |= TB_FLAG_CHERI_PCC_TOP_MAX;
+        if (cap_has_perms(ddc, CAP_PERM_STORE))
+            *cheri_flags |= TB_FLAG_CHERI_DDC_WRITABLE;
