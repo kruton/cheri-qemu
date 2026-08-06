CTSRD-CHERI/qemu

[Morello] Fix out-of-bounds access when translating RRLEN
Passing AS_ZERO() to cpu_reg() will translate 31 to 32 which is not a
valid argument for cpu_reg (it uses 31 for the zero register).
Found while running the Morello tests with ASAN.

target/arm/translate-cheri.inc.c
@@ -1595,7 +1595,7 @@ TRANS_F(RR)
     default: return false;
     }
 
-    helper(cpu_reg(ctx, AS_ZERO(a->Rd)), cpu_env, cpu_reg(ctx, AS_ZERO(a->Rn)));
+    helper(cpu_reg(ctx, a->Rd), cpu_env, cpu_reg(ctx, a->Rn));
     gen_lazy_cap_set_int(ctx, AS_ZERO(a->Rd));
 
     return true;

kruton/cheri-qemu

target/arm/translate-cheri.inc.c
@@ -54,3 +54,4 @@ TRANS_F(GC)
         gen_cap_get_perms(ctx, regnum, result);
     case 0b111: // type
         gen_cap_get_type(ctx, regnum, result);
+    helper(cpu_reg(ctx, a->Rd), tcg_env, cpu_reg(ctx, a->Rn));