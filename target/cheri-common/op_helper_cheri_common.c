CTSRD-CHERI/qemu

Fix register number reported on missing PERM_CINVOKE
This was found by the MIPS testsuite, but surprisingly not by 100 rounds
of testrig (since both implementations trap). This is something that
should be easy to detect in TestRIG once we report more detailed trap
information.

target/cheri-common/op_helper_cheri_common.c
@@ -274,9 +274,10 @@ void CHERI_HELPER_IMPL(cinvoke(CPUArchState *env, uint32_t code_regnum,
         raise_cheri_exception(env, CapEx_SealViolation, data_regnum);
     } else if ((code_cap->cr_otype != data_cap->cr_otype || code_cap->cr_otype > CAP_LAST_NONRESERVED_OTYPE)) {
         raise_cheri_exception(env, CapEx_TypeViolation, code_regnum);
-    } else if (!cap_has_perms(code_cap, CAP_PERM_CINVOKE) ||
-               !cap_has_perms(data_cap, CAP_PERM_CINVOKE)) {
+    } else if (!cap_has_perms(code_cap, CAP_PERM_CINVOKE)) {
         raise_cheri_exception(env, CapEx_PermitCCallViolation, code_regnum);
+    } else if (!cap_has_perms(data_cap, CAP_PERM_CINVOKE)) {
+        raise_cheri_exception(env, CapEx_PermitCCallViolation, data_regnum);
     } else if (!cap_has_perms(code_cap, CAP_PERM_EXECUTE)) {
         raise_cheri_exception(env, CapEx_PermitExecuteViolation, code_regnum);
     } else if (cap_has_perms(data_cap, CAP_PERM_EXECUTE)) {

kruton/cheri-qemu

target/cheri-common/op_helper_cheri_common.c
@@ -34,6 +34,9 @@
         // The return capability should always be a sentry
             cap_make_sealed_entry(&result);
 #ifdef TARGET_RISCV
+        raise_cheri_exception_branch(env, CapEx_SealViolation, data_regnum);
+    } else if (!cap_has_perms(code_cap, CAP_PERM_CINVOKE)) {
+    } else if (!cap_has_perms(data_cap, CAP_PERM_CINVOKE)) {
     } else if (!cap_is_unsealed(csp)) {
                                   target_ulong rt))
         raise_cheri_exception(env, CapEx_UserDefViolation, cs);