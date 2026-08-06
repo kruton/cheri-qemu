CTSRD-CHERI/qemu

[CHERI-RISC-V] CLoadTags zero register index is null register not DDC
We were treating CLoadTags with a zero base register index as a load
relative to DDC, but the sail-cheri-riscv implementation uses the null
register instead. TestRIG trace:
```
.4byte 0xff20085b # cloadtags x16, x0
.assert trap == 1 "should fault with a tag violation"
.4byte 0x34202873 # csrrs x16, mcause (0x342), x0
.assert rd_wdata == 0x1c "EXPECTED CHERI TRAP"
.4byte 0x343020f3 # csrrs x1, mtval (0x343), x0
.assert rd_wdata == 2 "cnull tag violation"
```

target/cheri-common/op_helper_cheri_common.c
@@ -1560,7 +1560,7 @@ target_ulong CHERI_HELPER_IMPL(cloadtags(CPUArchState *env, uint32_t cb))
     static const uint32_t sizealign = ncaps * CHERI_CAP_SIZE;
 
     GET_HOST_RETPC();
-    const cap_register_t *cbp = get_capreg_0_is_ddc(env, cb);
+    const cap_register_t *cbp = get_load_store_base_cap(env, cb);
 
     const target_ulong addr = cap_check_common_reg(
         perms, env, cb, 0, sizealign, _host_return_address, cbp, sizealign,

kruton/cheri-qemu

target/cheri-common/op_helper_cheri_common.c
@@ -210,6 +210,7 @@ void CHERI_HELPER_IMPL(cbuildcap(CPUArchState *env, uint32_t cd, uint32_t cb,
     update_capreg(env, cd, &result);
     GET_HOST_RETPC_IF_TRAPPING_CHERI_ARCH();
     DEFINE_RESULT_VALID;
+    const cap_register_t *cbp = get_readonly_capreg(env, cb);
     if (!cbp->cr_tag) {
         raise_cheri_exception_or_invalidate(env, CapEx_TagViolation, cb);
         raise_cheri_exception_or_invalidate(env, CapEx_SealViolation, cb);